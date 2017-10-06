17b62537c57168738fefe99cc489fadda9080d80
Alex Eagle <alexeagle@google.com>
2017-10-06 10:41:27 -0700

    use runfiles manifest in bazel version test

--- internal/check_bazel_version_test.py
+++ internal/check_bazel_version_test.py
@@ -1,6 +1,17 @@
 import os
 import unittest
 
+def resolve_runfile(path):
+  if os.getenv('RUNFILES_MANIFEST_ONLY') == "1":
+    manifest = os.getenv('RUNFILES_MANIFEST_FILE')
+    with open(manifest) as f:
+      for line in f.readlines():
+        if line.split()[0] == path:
+          return line.split()[1]
+    raise "Cannot find %s in manifest %s" % (path, manifest)
+  else:
+    return os.path.join(os.environ['TEST_SRCDIR'], path)
+
 
 class MockSkylark:
   """A class that returns mocked bazel version and fail().
@@ -26,7 +37,8 @@ class CheckBazelVersionTest(unittest.TestCase):
         'native': self.mock,
         'fail': self.mock.fail.__get__(self.mock, MockSkylark),
     }
-    execfile(os.path.join(os.environ['TEST_SRCDIR'], self.BZL_PATH), self.globals)
+
+    execfile(resolve_runfile(self.BZL_PATH), self.globals)
 
   def testVeryOldBazel(self):
     # Don't call setVersion, so there is no bazel_version property
