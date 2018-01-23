a45226426ff1e485e25d2507dc861e6e292e3500
Greg Magolan <gmagolan@gmail.com>
2018-01-22 16:19:15 -0800

    Handle module mappings in rollup.config.js and resolve to either workspace or external import

--- internal/e2e/rollup/rollup.spec.js
+++ internal/e2e/rollup/rollup.spec.js
@@ -5,7 +5,7 @@ describe('bundling', () => {
     // TODO(#32) Can shorten the path if https://github.com/bazelbuild/rules_nodejs/issues/32 is resolved
     const bundle = 'build_bazel_rules_nodejs/internal/e2e/rollup/bundle.min.js';
     const actual = fs.readFileSync(require.resolve(bundle), { encoding: 'utf-8' });
-    const expected = '(function(){"use strict";var name="Alice";console.log("Hello, "+name)})();';
+    const expected = '(function(){"use strict";var name="Alice";var fum="Wonderland";console.log("Hello, "+name+" in "+fum)})();';
     expect(actual).toEqual(expected);
   });
 });
--- internal/rollup/rollup.config.js
+++ internal/rollup/rollup.config.js
@@ -8,37 +8,47 @@ const nodeResolve = require('rollup-plugin-node-resolve/dist/rollup-plugin-node-
 const commonjs = require('rollup-plugin-commonjs/dist/rollup-plugin-commonjs.cjs');
 
 const path = require('path');
-const fs = require('fs');
 
 const binDirPath = "TMPL_bin_dir_path";
 const workspaceName = "TMPL_workspace_name";
 const buildFileDirname = "TMPL_build_file_dirname";
 const labelName = "TMPL_label_name";
+const moduleMappings = TMPL_module_mappings;
 
 class NormalizePaths {
   resolveId(importee, importer) {
     // process.cwd() is the execroot and ends up looking something like /.../2c2a834fcea131eff2d962ffe20e1c87/bazel-sandbox/872535243457386053/execroot/<workspace_name>
-    // from that path to the es6 output is <bin_dir_path>/<build_file_path>/<label_name>.es6
+    // from that path to the es6 output is <bin_dir_path>/<build_file_dirname>/<label_name>.es6
+    // from there, sources from the user's workspace are under <user_workspace_name>/<path_to_source>
+    // and sources from external workspaces are under <external_workspace_name>/<path_to_source>
     var resolved;
     if (importee.startsWith(`./`) || importee.startsWith(`../`)) {
       // relative import
       resolved = path.join(importer ? path.dirname(importer) : '', importee);
-    }
-    else {
-      // workspace import?
-      let es6CollectedPath = path.join(process.cwd(), binDirPath, buildFileDirname, `${labelName}.es6`, importee);
-      if (!es6CollectedPath.endsWith(".js")) es6CollectedPath += ".js";
-      if (fs.existsSync(es6CollectedPath)) {
-        resolved = es6CollectedPath;
+    } else if (importee.startsWith(`${workspaceName}/`)) {
+      // workspace import
+      resolved = path.join(process.cwd(), binDirPath, buildFileDirname, `${labelName}.es6`, importee);
+    } else {
+      // possible workspace import or external import if importee matches a module mapping
+      for (const k in moduleMappings) {
+        if (importee == k || importee.startsWith(`${k}/`)) {
+          // replace the root module name on a mappings match
+          var v = moduleMappings[k];
+          var userWorkspace = workspaceName;
+          if (v.startsWith('external/')) {
+            // for external workspaces, drop the 'external/' from the module mapping and clear the user workspace name
+            v = v.slice('external/'.length);
+            userWorkspace = '';
+          }
+          importee = path.join(userWorkspace, v, importee.slice(k.length+1))
+          resolved = path.join(process.cwd(), binDirPath, buildFileDirname, `${labelName}.es6`, importee);
+          break;
+        }
       }
     }
-
-    // add .js extension if needed
     if (resolved) {
-      if (!resolved.endsWith(".js")) {
-        resolved += ".js";
-      }
-      return resolved;
+      // resolve with node path resolution (it will handle index.js)
+      return require.resolve(resolved);
     }
   }
 }
