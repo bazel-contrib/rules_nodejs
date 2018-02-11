ba558d618906b8c293bc816f38e0429fd3c88c68
Greg Magolan <gmagolan@gmail.com>
2018-02-11 09:58:16 -0800

    Switch py_test to jasmine_node_test in examples/packages

--- examples/packages/version.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-var packageJsonPath = require.resolve('jsesc').replace('jsesc.js', 'package.json');
-var packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
-console.log(packageJson.version);
