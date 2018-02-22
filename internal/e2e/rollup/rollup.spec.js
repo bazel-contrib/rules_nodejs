e72723558e3eaf998e36af85be3b6ae543efa3d6
Alex Eagle <eagle@post.harvard.edu>
2018-02-21 18:14:39 -0800

    Add sourceMap config to uglify (#128)

--- internal/e2e/rollup/rollup.spec.js
+++ internal/e2e/rollup/rollup.spec.js
@@ -6,7 +6,11 @@ function read(relativePath) {
   // TODO(#32) Can shorten the path if
   // https://github.com/bazelbuild/rules_nodejs/issues/32 is resolved
   const path = `build_bazel_rules_nodejs/internal/e2e/rollup/${relativePath}`;
-  return fs.readFileSync(require.resolve(path), {encoding: 'utf-8'}).replace(/\r\n/g, '\n');
+  return fs.readFileSync(require.resolve(path), {encoding: 'utf-8'})
+      .replace(/\r\n/g, '\n')
+      .replace(/\\\\/g, '/')
+      // In the sourcemap, CRLF is turned into a string encoding
+      .replace(/\\r\\n/g, '\\n');
 }
 
 // These keys are arbitrary and local to this test.
@@ -43,6 +47,11 @@ Update the golden file:
 describe('bundling', () => {
   it('should work', () => {
     check('bundle.min.js', 'bundle-min_golden.js_');
+  });
+  it('should produce a sourcemap', () => {
+    // TODO(alexeagle): the right assertion is to load up the source-map library
+    // and assert that the sourcemap actually maps back to the sources
+    check('bundle.min.js.map', 'bundle-min_golden.js.map');
   });
   it('should produce a debug bundle', () => {
     check('bundle.min_debug.js', 'bundle-min-debug_golden.js_');
