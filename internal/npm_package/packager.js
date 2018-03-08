d18da613a1e7a5456915b2d459573cbdb0e5438a
Greg Magolan <gmagolan@gmail.com>
2018-03-08 07:36:05 -0800

    Handle case where BUILD_SCM_VERSION is not in the stamp file (#146)
    * Handle case where BUILD_SCM_VERSION is not in the stamp file
    
    * Same in rollup.config.js

--- internal/npm_package/packager.js
+++ internal/npm_package/packager.js
@@ -53,12 +53,14 @@ function main(args) {
     // BUILD_SCM_LOCAL_CHANGES true
     // BUILD_SCM_VERSION 6.0.0-beta.6+12.sha-83c699d.with-local-changes
     // BUILD_TIMESTAMP 1520021990506
-    const version = fs.readFileSync(stampFile, {encoding: 'utf-8'})
-                        .split('\n')
-                        .find(s => s.startsWith('BUILD_SCM_VERSION'))
-                        .split(' ')[1]
-                        .trim();
-    replacements.push([/0.0.0-PLACEHOLDER/g, version]);
+    const versionTag = fs.readFileSync(stampFile, {encoding: 'utf-8'})
+                           .split('\n')
+                           .find(s => s.startsWith('BUILD_SCM_VERSION'));
+    // Don't assume BUILD_SCM_VERSION exists
+    if (versionTag) {
+      const version = versionTag.split(' ')[1].trim();
+      replacements.push([/0.0.0-PLACEHOLDER/g, version]);
+    }
   }
   const rawReplacements = JSON.parse(replacementsArg);
   for (let key of Object.keys(rawReplacements)) {
