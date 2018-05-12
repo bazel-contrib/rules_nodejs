a21a6d19c40c31365cc3b0e47e1f3b3779cd9322
Greg Magolan <gmagolan@gmail.com>
2018-05-12 12:18:17 -0700

    Return proper bazel exit code from xjasmine_node_test on failure

--- internal/jasmine_node_test/xtest.sh
+++ internal/jasmine_node_test/xtest.sh
@@ -12,5 +12,8 @@ set -e
 
 if (( ${RESULT} == 0 )); then
   echo "Expected test to fail, but it succeeded" >&2
-  exit 1
+  # This exit code is handled specially by Bazel:
+  # https://github.com/bazelbuild/bazel/blob/486206012a664ecb20bdb196a681efc9a9825049/src/main/java/com/google/devtools/build/lib/util/ExitCode.java#L44
+  BAZEL_EXIT_TESTS_FAILED = 3;
+  exit ${BAZEL_EXIT_TESTS_FAILED}
 fi
