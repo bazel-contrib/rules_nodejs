c3a8a556a57abcd33a42af36b58d56bc8d5103ed
Alex Eagle <eagle@post.harvard.edu>
2018-06-05 17:03:41 -0700

    Handle .d.ts module roots in nodejs loader (#222)
    This allows libraries to have a nested structure where the index file is in a subdirectory of the package where the js_library/ts_library rule is declared.
    Fixes #220

---
+++ internal/e2e/node/lib1/src/index.js
@@ -0,0 +1,2 @@
+var foo_1 = require('./some');
+exports.a = foo_1.a;
--- internal/e2e/node/module_resolution.spec.js
+++ internal/e2e/node/module_resolution.spec.js
@@ -5,6 +5,8 @@ const node_resolve_index_4 = require('node_resolve_index_4');
 const node_resolve_main = require('node_resolve_main');
 const node_resolve_main_2 = require('node_resolve_main_2');
 const node_resolve_nested_main = require('node_resolve_nested_main');
+const lib1 = require('lib1');
+const lib1some = require('lib1/src/some');
 
 describe('node npm resolution', () => {
   it('should resolve to index.js by default', () => {
@@ -27,5 +29,11 @@ describe('node npm resolution', () => {
   });
   it('should resolve to main.js from a nested package.json', () => {
     expect(node_resolve_nested_main).toEqual('node_resolve_nested_main');
+  });
+  it('should resolve a nested index', () => {
+    expect(lib1.a).toEqual('lib1 content');
+  });
+  it('should be able to deep-import from a nested src dir', () => {
+    expect(lib1some.a).toEqual('lib1 content');
   });
 });
