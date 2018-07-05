5ea0589d07b633e2138d57b54b55f5bdc4da7c0e
Greg Magolan <gmagolan@gmail.com>
2018-07-05 15:23:32 -0700

    Added tests for node_modules_list and for proper dependency resolving for npm packages

---
+++ internal/e2e/node_loader_preserve_symlinks/node_loader_list_1_test.spec.js
@@ -0,0 +1,16 @@
+describe('node_loader_list_test', () => {
+  it('should resolve minimist', () => {
+    require('minimist');
+    expect(true).toEqual(true);
+  });
+
+  it('should resolve tmp', () => {
+    require('tmp');
+    expect(true).toEqual(true);
+  });
+
+  it('should resolve @gregmagolan/test-a to version 0.0.2', () => {
+    const testA = require('@gregmagolan/test-a');
+    expect(testA).toEqual('test-a-0.0.2');
+  });
+});
--- internal/e2e/node_loader_preserve_symlinks/node_loader_test.spec.js
+++ internal/e2e/node_loader_preserve_symlinks/node_loader_test.spec.js
@@ -12,4 +12,15 @@ describe('node_loader_test', () => {
     require('tmp');
     expect(true).toEqual(true);
   });
+
+  it('should resolve @gregmagolan/test-a to version 0.0.2', () => {
+    const testA = require('@gregmagolan/test-a');
+    expect(testA).toEqual('test-a-0.0.2');
+  });
+
+  it('should resolve @gregmagolan/test-b to version 0.0.2 with a @gregmagolan/test-a dependency of 0.0.1',
+     () => {
+       const testB = require('@gregmagolan/test-b');
+       expect(testB).toEqual('test-b-0.0.2/test-a-0.0.1');
+     });
 });
