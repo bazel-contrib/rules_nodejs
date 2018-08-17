116a44b799a406516901a67047138531af1d349a
Filipe Silva <filipematossilva@gmail.com>
2018-08-17 11:43:02 +0100

    Move data resolution specs into own file

---
+++ internal/e2e/node/data_resolution.spec.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+describe('node data resolution', () => {
+  const relativeDataPath = './data/data.json';
+
+  it('should be able to resolve data files through relative paths', () => {
+    const resolvedRelativeDataPath = require.resolve(relativeDataPath);
+    const dataContent = fs.readFileSync(resolvedRelativeDataPath);
+    expect(JSON.parse(dataContent)).toEqual({ "value": 42 });
+  });
+  it('should be able to resolve data files through absolute paths', () => {
+    const resolvedAbsoluteDataPath = require.resolve(path.join(__dirname, relativeDataPath));
+    const dataContent = fs.readFileSync(resolvedAbsoluteDataPath);
+    expect(JSON.parse(dataContent)).toEqual({ "value": 42 });
+  });
+  it('should throw when resolving files that are outside the sandbox', () => {
+    if (process.platform.startsWith('win')) {
+      // On Windows, file location is the original one and not sandboxed.
+      // This means we cannot correctly exclude relative paths that aren't part of the runfiles.
+      return;
+    }
+    // This file exists in the source folder but is not in the data array.
+    expect(() => require.resolve('./data/missing-data.json')).toThrow();
+  });
+});
