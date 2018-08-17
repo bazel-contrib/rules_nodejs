4ce0eb6b78d1524ec8dc2e3126cac8ff33f9e338
Filipe Silva <filipematossilva@gmail.com>
2018-08-17 12:06:52 +0100

    Add expectations for failing data resolution tests

--- internal/e2e/node/data_resolution.spec.js
+++ internal/e2e/node/data_resolution.spec.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 describe('node data resolution', () => {
   const relativeDataPath = './data/data.json';
+  const isBuiltFile = __filename.endsWith('_built.spec.js');
 
   it('should be able to resolve data files through relative paths', () => {
     const resolvedRelativeDataPath = require.resolve(relativeDataPath);
@@ -15,27 +16,30 @@ describe('node data resolution', () => {
     expect(JSON.parse(dataContent)).toEqual({ "value": 42 });
   });
   it('should throw when resolving files that are outside the sandbox', () => {
-    if (process.platform.startsWith('win')) {
-      // On Windows, file location is the original one and not sandboxed.
+    if (process.platform.startsWith('win') && !isBuiltFile) {
+      // On Windows, file location for source files is the original one and not sandboxed.
       // This means we cannot correctly exclude relative paths that aren't part of the runfiles.
-      return;
+      expect(() => require.resolve('./data/missing-data.json')).not.toThrow();
+    } else {
+      // This file exists in the source folder but is not in the data array.
+      expect(() => require.resolve('./data/missing-data.json')).toThrow();
     }
-    // This file exists in the source folder but is not in the data array.
-    expect(() => require.resolve('./data/missing-data.json')).toThrow();
   });
   it('should be able to resolve paths relative to data files', () => {
-    if (process.platform.startsWith('win')) {
-      // On Windows, file location is the original one and not sandboxed.
-      // This means we cannot resolve paths relative to data files back to built files.
-      return;
-    }
-
     const resolvedRelativeDataPath = require.resolve(relativeDataPath);
     const thisFilePath = __filename;
     const relativePathFromDataToThisFile = path.join('../', path.basename(thisFilePath));
-    const resolvedPathFromDataToThisFile = require.resolve(path.join(
-      path.dirname(resolvedRelativeDataPath), relativePathFromDataToThisFile));
+    const joinedPathFromDataToThisFile = path.join(path.dirname(resolvedRelativeDataPath), 
+      relativePathFromDataToThisFile)
 
-    expect(resolvedPathFromDataToThisFile).toEqual(thisFilePath);
+    if (process.platform.startsWith('win') && isBuiltFile) {
+      // On Windows, file location for data files is the original one and not sandboxed.
+      // This means we cannot resolve paths relative to data files back to built files.
+      expect(() => require.resolve(joinedPathFromDataToThisFile)).toThrow();
+    } else {
+      const resolvedPathFromDataToThisFile = require.resolve(joinedPathFromDataToThisFile);
+
+      expect(resolvedPathFromDataToThisFile).toEqual(thisFilePath);
+    }
   });
 });
