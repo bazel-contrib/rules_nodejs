4be219cab7d0c7dec420064775221f570937a5d5
Greg Magolan <gmagolan@gmail.com>
2018-09-13 17:27:51 -0700

    Fix jasmine_node_test spec filter to include '_' as word boundaries

---
+++ internal/jasmine_node_test/test/foo.spec.js
@@ -0,0 +1,5 @@
+describe('spec in file ending with .spec.js', () => {
+  it('should run', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ internal/jasmine_node_test/test/foo_test.js
@@ -0,0 +1,5 @@
+describe('spec in file ending with _test.js', () => {
+  it('should run', () => {
+    expect(true).toBe(true);
+  });
+});
