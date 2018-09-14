64fbda7f63f1e5add59bf31ab96125159e7628c9
Greg Magolan <gmagolan@gmail.com>
2018-09-14 10:49:58 -0700

    Update to rules_nodejs 0.13.1
    Closes #268
    
    PiperOrigin-RevId: 213008099

--- third_party/github.com/bazelbuild/rules_typescript/internal/e2e/default_tsconfig_test.js
+++ third_party/github.com/bazelbuild/rules_typescript/internal/e2e/default_tsconfig_test.js
@@ -24,9 +24,9 @@ const tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), 'wksp'));
 const WORKSPACE_BOILERPLATE = `
 http_archive(
     name = "build_bazel_rules_nodejs",
-    urls = ["https://github.com/bazelbuild/rules_nodejs/archive/0.12.4.zip"],
-    strip_prefix = "rules_nodejs-0.12.4",
-    sha256 = "c482700e032b4df60425cb9a6f8f28152fb1c4c947a9d61e6132fc59ce332b16"
+    urls = ["https://github.com/bazelbuild/rules_nodejs/archive/0.13.1.zip"],
+    strip_prefix = "rules_nodejs-0.13.1",
+    sha256 = "6e8ff3e59f15c7188d778a467f77c5d037da0bb97e44dfe7f4f9510415b36153",
 )
 http_archive(
     name = "bazel_skylib",
