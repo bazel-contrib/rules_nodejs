68c30bfc17187adbb016e8b601e6b5ca32844065
Greg Magolan <gmagolan@gmail.com>
2018-09-24 20:53:40 -0700

    Add some comments to document how test should be run

--- examples/rollup/rollup.spec.js
+++ examples/rollup/rollup.spec.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 
 describe('bundling', () => {
-  it('should accept vars', () => {
+  it('should accept vars; must be run with --define=some_env=some_value', () => {
+    // This test must be run with --define=some_env=some_value.
+    // Use `bazel test ... --define=some_env=some_value` or
+    // `bazel test //examples/rollup:test --define=some_env=some_value`.
     expect(process.env['some_env']).toEqual('some_value');
   });
   it('should work', () => {
