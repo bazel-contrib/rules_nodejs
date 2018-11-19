c1a0987105b390f3e5cecfbc9e702bf5adabaa12
Greg Magolan <gmagolan@gmail.com>
2018-11-19 12:02:03 -0800

    Make BUILD file check in fine grained deps case insensitive (#424)

--- internal/e2e/fine_grained_deps/common.spec.js
+++ internal/e2e/fine_grained_deps/common.spec.js
@@ -15,6 +15,13 @@ describe('dependencies', () => {
     require('ajv/lib/$data');
   });
 
+  it(`should resolve rxjs/src/tsconfig.json`, () => {
+    // the BUILD.bazel file in rxjs/src should have been
+    // deleted by fine grained deps and rxjs/src/tsconfig.json
+    // should be available in the runfiles
+    require('rxjs/src/tsconfig.json');
+  });
+
   it(`should resolve @gregmagolan/test-b to version 0.0.2 with a @gregmagolan/test-a dependency of 0.0.1
   Note that @gregmagolan/test-a@0.0.2 is an explicit devDependency of this project,
   so we are really testing that test-b will get the version it depends on, not
