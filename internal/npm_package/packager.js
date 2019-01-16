be0252b02667d4b1c01102449220ed98fc170256
Greg Magolan <gmagolan@gmail.com>
2019-01-16 08:55:05 -0800

    Don't do replacements on archive files in npm_package

--- internal/npm_package/packager.js
+++ internal/npm_package/packager.js
@@ -17,6 +17,119 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * Returns true if a file is detected to be binary.
+ *
+ * This implementation is a slightly modified version of
+ * isBinaryFileSync() from https://github.com/gjtorikian/isBinaryFile.
+ * The MIT license is included at the top of the function body below.
+ */
+function isBinary(p) {
+  // Copyright (c) 2019 Garen J. Torikian
+  //
+  // MIT License
+  //
+  // Permission is hereby granted, free of charge, to any person obtaining
+  // a copy of this software and associated documentation files (the
+  // "Software"), to deal in the Software without restriction, including
+  // without limitation the rights to use, copy, modify, merge, publish,
+  // distribute, sublicense, and/or sell copies of the Software, and to
+  // permit persons to whom the Software is furnished to do so, subject to
+  // the following conditions:
+  //
+  // The above copyright notice and this permission notice shall be
+  // included in all copies or substantial portions of the Software.
+  //
+  // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  // NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  // LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  const MAX_BYTES = 512
+  const fd = fs.openSync(p, 'r');
+  const buf = Buffer.alloc(MAX_BYTES);
+  const bytes = fs.readSync(fd, buf, 0, MAX_BYTES, 0);
+  fs.closeSync(fd);
+
+  // empty file. no clue what it is.
+  if (bytes === 0) {
+    return false;
+  }
+
+  let suspiciousBytes = 0;
+  const totalBytes = Math.min(bytes, MAX_BYTES);
+
+  // UTF-8 BOM
+  if (bytes >= 3 && buf[0] === 0xef && buf[1] === 0xbb && buf[2] === 0xbf) {
+    return false;
+  }
+
+  // UTF-32 BOM
+  if (bytes >= 4 && buf[0] === 0x00 && buf[1] === 0x00 && buf[2] === 0xfe && buf[3] === 0xff) {
+    return false;
+  }
+
+  // UTF-32 LE BOM
+  if (bytes >= 4 && buf[0] === 0xff && buf[1] === 0xfe && buf[2] === 0x00 && buf[3] === 0x00) {
+    return false;
+  }
+
+  // GB BOM
+  if (bytes >= 4 && buf[0] === 0x84 && buf[1] === 0x31 && buf[2] === 0x95 && buf[3] === 0x33) {
+    return false;
+  }
+
+  if (totalBytes >= 5 && buf.slice(0, 5).toString() === '%PDF-') {
+    /* PDF. This is binary. */
+    return true;
+  }
+
+  // UTF-16 BE BOM
+  if (bytes >= 2 && buf[0] === 0xfe && buf[1] === 0xff) {
+    return false;
+  }
+
+  // UTF-16 LE BOM
+  if (bytes >= 2 && buf[0] === 0xff && buf[1] === 0xfe) {
+    return false;
+  }
+
+  for (let i = 0; i < totalBytes; i++) {
+    if (buf[i] === 0) {
+      // NULL byte--it's binary!
+      return true;
+    } else if ((buf[i] < 7 || buf[i] > 14) && (buf[i] < 32 || buf[i] > 127)) {
+      // UTF-8 detection
+      if (buf[i] > 193 && buf[i] < 224 && i + 1 < totalBytes) {
+        i++;
+        if (buf[i] > 127 && buf[i] < 192) {
+          continue;
+        }
+      } else if (buf[i] > 223 && buf[i] < 240 && i + 2 < totalBytes) {
+        i++;
+        if (buf[i] > 127 && buf[i] < 192 && buf[i + 1] > 127 && buf[i + 1] < 192) {
+          i++;
+          continue;
+        }
+      }
+
+      suspiciousBytes++;
+      // Read at least 32 buf before making a decision
+      if (i > 32 && (suspiciousBytes * 100) / totalBytes > 10) {
+        return true;
+      }
+    }
+  }
+
+  if ((suspiciousBytes * 100) / totalBytes > 10) {
+    return true;
+  }
+
+  return false;
+}
+
 function mkdirp(p) {
   if (!fs.existsSync(p)) {
     mkdirp(path.dirname(p));
@@ -24,13 +137,18 @@ function mkdirp(p) {
   }
 }
 
-function write(p, content, replacements) {
-  mkdirp(path.dirname(p));
-  replacements.forEach(r => {
-    const [regexp, newvalue] = r;
-    content = content.replace(regexp, newvalue);
-  });
-  fs.writeFileSync(p, content);
+function copyWithReplace(src, dest, replacements) {
+  mkdirp(path.dirname(dest));
+  if (!isBinary(src)) {
+    let content = fs.readFileSync(src, {encoding: 'utf-8'});
+    replacements.forEach(r => {
+      const [regexp, newvalue] = r;
+      content = content.replace(regexp, newvalue);
+    });
+    fs.writeFileSync(dest, content);
+  } else {
+    fs.copyFileSync(src, dest);
+  }
 }
 
 function unquoteArgs(s) {
@@ -72,9 +190,7 @@ function main(args) {
 
   // src like baseDir/my/path is just copied to outDir/my/path
   for (src of srcsArg.split(',').filter(s => !!s)) {
-    const content = fs.readFileSync(src, {encoding: 'utf-8'});
-    const outPath = path.join(outDir, path.relative(baseDir, src));
-    write(outPath, content, replacements);
+    copyWithReplace(src, path.join(outDir, path.relative(baseDir, src)), replacements);
   }
 
   function outPath(f) {
@@ -100,12 +216,15 @@ function main(args) {
   // by users outside of the package.
   for (dep of depsArg.split(',').filter(s => !!s && !s.startsWith('external/'))) {
     try {
-      const content = fs.readFileSync(dep, {encoding: 'utf-8'});
-      write(outPath(dep), content, replacements);
+      copyWithReplace(dep, outPath(dep), replacements);
     } catch (e) {
       console.error(`Failed to copy ${dep} to ${outPath(dep)}`);
       throw e;
     }
+  }
+
+  for (dep of depsArg.split(',').filter(s => !!s)) {
+    
   }
 
   // package contents like bazel-bin/baseDir/my/directory/* is
@@ -119,8 +238,7 @@ function main(args) {
           copyRecursive(base, path.join(file, f));
         });
       } else {
-        const content = fs.readFileSync(path.join(base, file), {encoding: 'utf-8'});
-        write(path.join(outDir, file), content, replacements);
+        copyWithReplace(path.join(base, file), path.join(outDir, file), replacements);
       }
     }
     fs.readdirSync(pkg).forEach(f => {
