98eb1c10f05f89e53cc0d07205d25a186529d244
Greg Magolan <gmagolan@gmail.com>
2019-01-20 23:45:03 -0800

    Favor dev version of npm bazel workspaces

--- internal/e2e/bazel_workspaces_compat/npm/test_workspace_b/src/test.spec.js
+++ internal/e2e/bazel_workspaces_compat/npm/test_workspace_b/src/test.spec.js
@@ -1,7 +1,5 @@
 console.log('test_workspace@0.0.2');
 
-describe('should', () => {
-  it('run', () => {
-    expect(1).toBe(1);
-  });
+describe('should not', () => {
+  it('run', () => {fail('should not be loaded')});
 });
