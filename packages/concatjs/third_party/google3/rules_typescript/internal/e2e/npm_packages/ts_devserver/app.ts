f248dc656730d8e499814340ff2854cf78c712d9
Greg Magolan <gmagolan@gmail.com>
2019-02-04 12:34:39 -0800

    Add e2e/npm_packages/ts_devserver test
    Fix npm script syntax to run on Windows
    
    Closes #401
    
    PiperOrigin-RevId: 232346165

---
+++ packages/concatjs/third_party/google3/rules_typescript/internal/e2e/npm_packages/ts_devserver/app.ts
@@ -0,0 +1,4 @@
+const el: HTMLDivElement = document.createElement('div');
+el.innerText = 'Hello, TypeScript';
+el.className = 'ts1';
+document.body.appendChild(el);
---
+++ packages/concatjs/third_party/google3/rules_typescript/internal/e2e/npm_packages/ts_devserver/protractor.conf.js
@@ -0,0 +1,12 @@
+exports.config = {
+  specs: [
+    'bazel-bin/*_e2e_test.js',
+  ],
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {args: ['--no-sandbox']}
+  },
+  directConnect: true,
+  baseUrl: 'http://localhost:8080/',
+  framework: 'jasmine',
+};
