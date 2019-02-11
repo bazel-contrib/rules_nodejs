efdc184540c0b2bd855dff26eb80842c0ce3f35e
Greg Magolan <gmagolan@gmail.com>
2019-02-11 14:30:14 -0800

    Don't run vendored_node test unless local system is Linux (#541)
    * Don't run vendored_node test unless local system is Linux
    
    * Cleanup after review

--- examples/test_examples.sh
+++ examples/test_examples.sh
@@ -4,11 +4,17 @@
 # These examples should be convenient for users to reference, not exercise all our features.
 
 set -u -e -o pipefail
-cd $(dirname $0)
+
+TESTS_ROOT_DIR=$(cd $(dirname "$0"); pwd)
+NODEJS_ROOT_DIR=$(cd $TESTS_ROOT_DIR/..; pwd)
+
+KERNEL_NAME=$(uname -s)
 
 # Make sure the distro is up-to-date and can be referenced at bazel-bin/rules_nodejs_package
+cd $NODEJS_ROOT_DIR
 bazel build --symlink_prefix=bazel- //:local_testing_package
 
+cd $TESTS_ROOT_DIR
 for testDir in $(ls) ; do
   [[ -d "$testDir" ]] || continue
   (  
@@ -17,6 +23,10 @@ for testDir in $(ls) ; do
     echo "#################################################################################"
     echo "Testing example in $(pwd)"
     echo ""
-    yarn test
+    if [[ $testDir == "vendored_node" && $KERNEL_NAME != Linux* ]] ; then
+      echo "Skipping vendored_node test as it only runs on Linux while we are executing on $KERNEL_NAME"
+    else
+      yarn test
+    fi
   )
 done
