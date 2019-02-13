fe4925cac4c70b3782437609b08fb00561093d13
Greg Magolan <gmagolan@gmail.com>
2019-02-12 19:28:14 -0800

    Fix fs.copyFileSync `EACCES: permission denied` failure (#546)
    * Fix for web_package fs.copyFileSync issue
    
    * Address review comment

--- internal/web_package/assembler.js
+++ internal/web_package/assembler.js
@@ -70,7 +70,12 @@ function main(params) {
     }
   }
 
-  for (const f of params) {
+  // Remove duplicate files (which may come from this rule) from the
+  // list since fs.copyFileSync may fail with `EACCES: permission denied`
+  // as it will not have permission to overwrite duplicate files that were
+  // copied from within bazel-bin.
+  // See https://github.com/bazelbuild/rules_nodejs/pull/546.
+  for (const f of new Set(params)) {
     copy(f);
   }
   return 0;
