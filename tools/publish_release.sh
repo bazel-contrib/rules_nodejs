517e63e331999a00585e96f80e79b1e245751099
Alex Eagle <alexeagle@google.com>
2019-02-24 14:14:05 -0800

    Introduce @bazel/labs package.
    This follows the same rationale as @angular/labs.
    I imagine things like the Webpack plugin should go here until they reach stability

--- tools/publish_release.sh
+++ tools/publish_release.sh
@@ -13,7 +13,7 @@ readonly NPM_COMMAND=${NPM_COMMAND:-publish}
 # Bazel can't know if the git metadata changed
 readonly TMP=$(mktemp -d -t bazel-release.XXXXXXX)
 
-for pkg in jasmine typescript karma; do (
+for pkg in jasmine typescript karma labs; do (
     cd packages/$pkg
     bazel --output_base=$TMP run  --workspace_status_command=../../tools/current_version.sh //:npm_package.${NPM_COMMAND}
 ) done
