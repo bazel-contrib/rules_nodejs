95e90da8e65dd8c5a3973ce18895b25e714d49fc
radokirov <radokirov@google.com>
2019-02-28 16:24:34 -0800

    Move the tsickle auto-quoting logic into a tsetse check.
    This check is only useful for code that would be opmitized through a property
    renaming optimizer (like Closure, and experiemental uglifyJS option, AFAIK).
    
    It is intentionally, exposed but not turned on by default for rules_typescript.
    
    PiperOrigin-RevId: 236218830

--- third_party/github.com/bazelbuild/rules_typescript/internal/tsetse/error_code.ts
+++ third_party/github.com/bazelbuild/rules_typescript/internal/tsetse/error_code.ts
@@ -1,7 +1,15 @@
+/**
+ * Error codes for tsetse checks.
+ *
+ * Start with 21222 and increase linearly.
+ * The intent is for these codes to be fixed, so that tsetse users can
+ * search for them in user forums and other media.
+ */
 export enum ErrorCode {
   CHECK_RETURN_VALUE = 21222,
   EQUALS_NAN = 21223,
   BAN_EXPECT_TRUTHY_PROMISE = 21224,
   MUST_USE_PROMISES = 21225,
   BAN_PROMISE_AS_CONDITION = 21226,
+  PROPERTY_RENAMING_SAFE = 21227,
 }
