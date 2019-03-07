a87249f4e6ca4bf29dbe2e68186288291fa70c55
Alex Eagle <alexeagle@angular.io>
2019-03-07 13:13:01 -0800

    fix(@bazel/typescript): remove unknown rules from generated API doc
    Hopefully this fixes it so npmjs will render the README

---
+++ packages/typescript/docs/docs_test.js
@@ -0,0 +1,5 @@
+const actual = require('fs').readFileSync(
+    process.env['TEST_SRCDIR'] + '/npm_bazel_typescript/docs/index.md', {encoding: 'utf-8'});
+if (actual.indexOf('<unknown name>') >= 0) {
+  throw new Error('Found <unknown name> in index.md');
+}
