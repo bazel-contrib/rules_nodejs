e881244124ed9bdbccd3dc0bc47e2284903bcfe8
Greg Magolan <gmagolan@gmail.com>
2019-04-07 22:19:57 -0700

    Fix yarn & npm determinism tests in /internal/e2e/packages

--- internal/e2e/packages/npm_determinism.spec.js
+++ internal/e2e/packages/npm_determinism.spec.js
@@ -7,7 +7,7 @@ const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
 const packageJsonPath2 = packageJsonPath.replace(
     '/packages_example_npm_install_duplicate_for_determinism_testing/',
     '/packages_example_npm_install/');
-const packageJson2 = JSON.parse(fs.readFileSync(packageJsonPath));
+const packageJson2 = JSON.parse(fs.readFileSync(packageJsonPath2));
 
 describe('jsesc package.json files', () => {
   it('jsesc package.json files from two different npm_install runs should have the same contents',
