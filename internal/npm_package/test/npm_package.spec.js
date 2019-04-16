b4d4bbfb7979c03387f52fbc38426d36faf079f6
Laszlo Csomor <laszlocsomor@google.com>
2019-04-16 16:20:35 +0200

    Fix //internal/npm_package/test:test
    Change the file npm_package.spec.js was locating,
    from "test.sh" to just "test".

--- internal/npm_package/test/npm_package.spec.js
+++ internal/npm_package/test/npm_package.spec.js
@@ -8,7 +8,7 @@ function read(p) {
   // So instead we lookup the sibling file (the primary output of the test rule)
   // and bootstrap the filesystem lookup from there.
   const dir =
-      path.dirname(require.resolve('build_bazel_rules_nodejs/internal/npm_package/test/test.sh'));
+      path.dirname(require.resolve('build_bazel_rules_nodejs/internal/npm_package/test/test'));
   return fs.readFileSync(path.join(dir, 'test_pkg', p), {encoding: 'utf-8'}).trim();
 }
 
