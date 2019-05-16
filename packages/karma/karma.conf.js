c92fbef94252dec194b757e91a3d409cdf7a2c2d
Greg Magolan <gmagolan@gmail.com>
2019-05-16 09:43:10 -0700

    Update @bazel/karma to latest sable Chromium 74 for osx & linux (#766)
    OSX Chromium 73+ requires disabling of the sandbox under Bazel
    Windows still on Chromium 66 and updating that is a separate task and requires changes to rules_webtesting as the archive name & executable path has changed

--- packages/karma/karma.conf.js
+++ packages/karma/karma.conf.js
@@ -60,31 +60,51 @@ try {
   }
 
   /**
-   * Chrome on Linux uses sandboxing, which needs user namespaces to be enabled.
-   * This is not available on all kernels and it might be turned off even if it is available.
-   * Notable examples where user namespaces are not available include:
-   * - In Debian it is compiled-in but disabled by default.
-   * - The Docker daemon for Windows or OSX does not support user namespaces.
-   * We can detect if user namespaces are supported via /proc/sys/kernel/unprivileged_userns_clone.
-   * For more information see:
-   * https://github.com/Googlechrome/puppeteer/issues/290
-   * https://superuser.com/questions/1094597/enable-user-namespaces-in-debian-kernel#1122977
-   * https://github.com/karma-runner/karma-chrome-launcher/issues/158
-   * https://github.com/angular/angular/pull/24906
+   * Check if Chrome sandboxing is supported on the current platform.
    */
-  function supportsSandboxing() {
-    if (process.platform !== 'linux') {
-      return true;
-    }
-    try {
-      const res = child_process.execSync('cat /proc/sys/kernel/unprivileged_userns_clone')
-                      .toString()
-                      .trim();
-      return res === '1';
-    } catch (error) {
+  function supportChromeSandboxing() {
+    if (process.platform === 'darwin') {
+      // Chrome 73+ fails to initialize the sandbox on OSX when running under Bazel.
+      // ```
+      // ERROR [launcher]: Cannot start ChromeHeadless
+      // ERROR:crash_report_database_mac.mm(96)] mkdir
+      // /private/var/tmp/_bazel_greg/62ef096b0da251c6d093468a1efbfbd3/execroot/angular/bazel-out/darwin-fastbuild/bin/external/io_bazel_rules_webtesting/third_party/chromium/chromium.out/chrome-mac/Chromium.app/Contents/Versions/73.0.3683.0/Chromium
+      // Framework.framework/Versions/A/new: Permission denied (13) ERROR:file_io.cc(89)]
+      // ReadExactly: expected 8, observed 0 ERROR:crash_report_database_mac.mm(96)] mkdir
+      // /private/var/tmp/_bazel_greg/62ef096b0da251c6d093468a1efbfbd3/execroot/angular/bazel-out/darwin-fastbuild/bin/external/io_bazel_rules_webtesting/third_party/chromium/chromium.out/chrome-mac/Chromium.app/Contents/Versions/73.0.3683.0/Chromium
+      // Framework.framework/Versions/A/new: Permission denied (13) Chromium Helper[94642] <Error>:
+      // SeatbeltExecServer: Failed to initialize sandbox: -1 Operation not permitted Failed to
+      // initialize sandbox. [0213/201206.137114:FATAL:content_main_delegate.cc(54)] Check failed:
+      // false. 0   Chromium Framework                  0x000000010c078bc9 ChromeMain + 43788137 1
+      // Chromium Framework                  0x000000010bfc0f43 ChromeMain + 43035363
+      // ...
+      // ```
+      return false;
     }
 
-    return false;
+    if (process.platform === 'linux') {
+      // Chrome on Linux uses sandboxing, which needs user namespaces to be enabled.
+      // This is not available on all kernels and it might be turned off even if it is available.
+      // Notable examples where user namespaces are not available include:
+      // - In Debian it is compiled-in but disabled by default.
+      // - The Docker daemon for Windows or OSX does not support user namespaces.
+      // We can detect if user namespaces are supported via
+      // /proc/sys/kernel/unprivileged_userns_clone. For more information see:
+      // https://github.com/Googlechrome/puppeteer/issues/290
+      // https://superuser.com/questions/1094597/enable-user-namespaces-in-debian-kernel#1122977
+      // https://github.com/karma-runner/karma-chrome-launcher/issues/158
+      // https://github.com/angular/angular/pull/24906
+      try {
+        const res = child_process.execSync('cat /proc/sys/kernel/unprivileged_userns_clone')
+                        .toString()
+                        .trim();
+        return res === '1';
+      } catch (error) {
+      }
+      return false;
+    }
+
+    return true;
   }
 
   /**
@@ -324,7 +344,7 @@ try {
             process.env.CHROME_BIN = require.resolve(webTestNamedFiles['CHROMIUM']);
           }
           const browser = process.env['DISPLAY'] ? 'Chrome' : 'ChromeHeadless';
-          if (!supportsSandboxing()) {
+          if (!supportChromeSandboxing()) {
             const launcher = 'CustomChromeWithoutSandbox';
             conf.customLaunchers = {[launcher]: {base: browser, flags: ['--no-sandbox']}};
             conf.browsers.push(launcher);
