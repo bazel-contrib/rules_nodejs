1f781e85d1acb9b0e0eb949013430bedabf2bd02
Alex Eagle <alexeagle@angular.io>
2019-05-26 10:09:25 -0700

    Create npm_install unless we run under yarn create

--- packages/create/index.js
+++ packages/create/index.js
@@ -1,5 +1,25 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const path = require('path');
+const DEBUG = false;
+
+/**
+ * Detect if the user ran `yarn create @bazel` so we can default
+ * to using yarn in the new workspace.
+ *
+ * TODO: check whether it detects properly on Windows
+ */
+function detectRunningUnderYarn() {
+  const executable = process.argv[1];
+  if (!!executable && executable.indexOf('yarn/bin') >= 0) {
+    return true;
+  }
+  if (process.env['_'] && process.env['_'].endsWith('yarn')) {
+    return true;
+  }
+  return false;
+}
 
 function main(args, error = console.error, log = console.log) {
   if (!args || args.length < 1) {
@@ -11,6 +31,17 @@ function main(args, error = console.error, log = console.log) {
         `);
     return 1;
   }
+
+  // Which package manager will be used in the new project
+  // TODO: make it a command line arg
+  // but don't make yargs a transitive dep because of global install
+  const pkgMgr = detectRunningUnderYarn() ? 'yarn' : 'npm';
+
+  if (DEBUG) {
+    log('Running with', process.argv);
+    log('Environment', process.env);
+  }
+
   // FIXME: the workspace name should conform with Bazel rules
   const [wkspDir] = args;
   // TODO: user might want these to differ
@@ -23,6 +54,20 @@ function main(args, error = console.error, log = console.log) {
         path.join(wkspDir, workspaceRelativePath), content + require('os').EOL,
         {encoding: 'utf-8'});
   }
+
+  const yarnInstallCmd = `load("@build_bazel_rules_nodejs//:defs.bzl", "yarn_install")
+yarn_install(
+    name = "npm",
+    package_json = "//:package.json",
+    yarn_lock = "//:yarn.lock",
+)`;
+
+  const npmInstallCmd = `load("@build_bazel_rules_nodejs//:defs.bzl", "npm_install")
+npm_install(
+    name = "npm",
+    package_json = "//:package.json",
+    package_lock_json = "//:package-lock.json",
+)`;
 
   write('WORKSPACE', `# Bazel workspace created by @bazel/create
 
@@ -43,12 +88,7 @@ http_archive(
     urls = ["https://github.com/bazelbuild/rules_nodejs/releases/download/0.30.1/rules_nodejs-0.30.1.tar.gz"],
 )
 
-load("@build_bazel_rules_nodejs//:defs.bzl", "yarn_install")
-yarn_install(
-    name = "npm",
-    package_json = "//:package.json",
-    yarn_lock = "//:yarn.lock",
-)
+${pkgMgr === 'yarn' ? yarnInstallCmd : npmInstallCmd}
 
 load("@npm//:install_bazel_dependencies.bzl", "install_bazel_dependencies")
 install_bazel_dependencies()`);
@@ -68,18 +108,31 @@ install_bazel_dependencies()`);
           },
           null, 4));
   // in the published distribution, this file will appear in the same folder as this file
-  if (fs.existsSync('common.bazelrc')) {
-    write('.bazelrc', fs.readFileSync('common.bazelrc'))
-  }
-  // but running locally against sources, it's in the root of the repo two directories up
-  if (fs.existsSync('../../common.bazelrc')) {
-    write('.bazelrc', fs.readFileSync('../../common.bazelrc'));
-  } else {
-    error('ERROR: missing common.bazelrc file, continuing with no bazel settings...');
+  try {
+    const rc = require.resolve('./common.bazelrc');
+    write('.bazelrc', fs.readFileSync(rc));
+  } catch (_) {
+    // but running locally against sources, it's in the root of the repo two directories up
+    if (fs.existsSync('../../common.bazelrc')) {
+      write('.bazelrc', fs.readFileSync('../../common.bazelrc'));
+    } else {
+      error('ERROR: missing common.bazelrc file, continuing with no bazel settings...');
+    }
   }
 
-  log(`Successfully created new Bazel workspace at ${path.resolve(wkspDir)}
-    Inside that directory you can run yarn build or yarn test.`)
+  log(`Successfully created new Bazel workspace at ${path.resolve(wkspDir)}`);
+  // TODO: we should probably run the package manager install now
+  if (pkgMgr === 'yarn') {
+    log(`Now in that directory, you can run
+    yarn install
+    yarn build
+    yarn test`);
+  } else {
+    log(`Now in that directory, you can run
+    npm install
+    npm run build
+    npm test`);
+  }
 }
 
 module.exports = {main};
