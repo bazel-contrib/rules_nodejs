918d6c4851eae578089e4812a7a35b426fa32b57
Alex Eagle <alexeagle@angular.io>
2019-05-26 10:09:25 -0700

    Create npm_install unless we run under yarn create

--- packages/create/test.js
+++ packages/create/test.js
@@ -28,3 +28,15 @@ const projFiles = fs.readdirSync('some-project');
 if (!projFiles.indexOf('.bazelrc') < 0) {
   fail('project should have .bazelrc');
 }
+const wkspContent = fs.readFileSync('some-project/WORKSPACE');
+if (wkspContent.indexOf('npm_install') < 0) {
+  fail('should use npm by default');
+}
+// TODO: run bazel in the new directory to verify a build works
+
+process.env['_'] = '/usr/bin/yarn';
+main(['default-to-yarn']);
+if (fs.readFileSync('default-to-yarn/WORKSPACE').indexOf('yarn_install') < 0) {
+  fail('should use yarn by default')
+}
+// TODO: run bazel in the new directory to verify a build works
