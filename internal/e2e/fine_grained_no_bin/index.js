58708707670f081ff4acbead00b1505b42dcc0bb
Markus Padourek <markus.padourek@gmail.com>
2019-05-29 13:33:02 +0200

    Remove old code.

--- internal/e2e/fine_grained_no_bin/index.js
+++ internal/e2e/fine_grained_no_bin/index.js
@@ -1,5 +1,1 @@
-const path = require("path");
-const Module = require("module");
-
-console.log('hello ' + require.resolve("./test.js", { paths: [ path.join(__dirname, 'test') ] }));
-console.log('hello ' + Module._resolveFilename("test", { paths: [ path.join(__dirname, 'test', 'node_modules') ] }));
+console.log('hello World');
