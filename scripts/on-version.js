0b04ecf3ff2db3185ea8261e995a38b7352a98f0
Alex Eagle <eagle@post.harvard.edu>
2019-06-11 14:52:17 -0700

    chore: automate more release process

--- scripts/on-version.js
+++ scripts/on-version.js
@@ -1,6 +1,6 @@
 // Called from "version" npm script when running `npm version`
 // during release process. This script updates the version
-// in package.bzl to match that of package.json.
+// in defs.bzl to match that of package.json.
 const shell = require('shelljs');
 const version = require('../package.json').version;
 
