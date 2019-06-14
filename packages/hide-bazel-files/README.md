e7d2fbd8d6c0b3bc27a6eeba59a4b08130ee8076
Greg Magolan <gmagolan@gmail.com>
2019-06-13 18:09:46 -0700

    fix(builtin): add @bazel/hide-bazel-files utility

---
+++ packages/hide-bazel-files/README.md
@@ -0,0 +1,24 @@
+# @bazel/hide-bazel-files
+
+A tool to hide Bazel files that may be shipped with some npm packages. Packages with these files cause build failures when used with `npm_install` or `yarn_install`.
+
+This tool renames all `BUILD` and `BUILD.bazel` files under node_modules to `_BUILD` and `_BUILD.bazel` respectively.
+
+If you see an error such as
+
+```
+ERROR: /private/var/tmp/_bazel_greg/37b273501bbecefcf5ce4f3afcd7c47a/external/npm/BUILD.bazel:9:1: Label '@npm//:node_modules/rxjs/src/AsyncSubject.ts' crosses boundary of subpackage '@npm//node_modules/rxjs/src' (perhaps you meant to put the colon here: '@npm//node_modules/rxjs/src:AsyncSubject.ts'?)
+```
+
+then chances are there is an npm package in your dependencies that contains a `BUILD` file. To resolve this, add `@bazel/hide-bazel-files` to your `devDependencies` and `hide-bazel-files` to your `postinstall` script like so:
+
+```
+"devDependencies": {
+  "@bazel/hide-bazel-files": "0.0.0-PLACEHOLDER"
+},
+"scripts": {
+  "postinstall": "hide-bazel-files"
+}
+```
+
+Note: The commonly used npm package rxjs contains `BUILD` files from version 5.5.5 to 6.4.0 inclusive. These have now been removed in version 6.5.0. If you are using an rxjs version in that range and that is the only npm package in your dependencies that contains `BUILD` files then you can try upgrading to rxjs 6.4.0 instead of using `hide-bazel-files`.
---
+++ packages/hide-bazel-files/index.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+function findBazelFiles(dir) {
+  return fs.readdirSync(dir).reduce((files, file) => {
+    const fullPath = path.posix.join(dir, file);
+    const isSymbolicLink = fs.lstatSync(fullPath).isSymbolicLink();
+    let stat;
+    try {
+      stat = fs.statSync(fullPath);
+    } catch (e) {
+      if (isSymbolicLink) {
+        // Filter out broken symbolic links. These cause fs.statSync(fullPath)
+        // to fail with `ENOENT: no such file or directory ...`
+        return files;
+      }
+      throw e;
+    }
+    const isDirectory = stat.isDirectory();
+    if (isDirectory && isSymbolicLink) {
+      // Filter out symbolic links to directories. An issue in yarn versions
+      // older than 1.12.1 creates symbolic links to folders in the .bin folder
+      // which leads to Bazel targets that cross package boundaries.
+      // See https://github.com/bazelbuild/rules_nodejs/issues/428 and
+      // https://github.com/bazelbuild/rules_nodejs/issues/438.
+      // This is tested in internal/e2e/fine_grained_symlinks.
+      return files;
+    }
+    if (isDirectory) {
+      return files.concat(findBazelFiles(fullPath));
+    } else {
+      const fileUc = file.toUpperCase();
+      if (fileUc == 'BUILD' || fileUc == 'BUILD.BAZEL') {
+        return files.concat(fullPath);
+      }
+      return files;
+    }
+  }, []);
+}
+
+function main() {
+  // Rename all bazel files found by prefixing them with `_`
+  for (f of findBazelFiles('node_modules')) {
+    const d = path.posix.join(path.dirname(f), `_${path.basename(f)}`);
+    fs.renameSync(f, d);
+  }
+  return 0;
+}
+
+module.exports = {main};
+
+if (require.main === module) {
+  process.exitCode = main();
+}
