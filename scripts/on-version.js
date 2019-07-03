1a65d29d66e041890e5eaf740f7c0419993f0bb4
Michael Yali <mihon4ik@gmail.com>
2019-07-03 15:53:38 +0300

    chore: update version in README.md
    Modified `scripts/on-version.js` to update version in README.md on `version` command
    
    Fixes #469

--- scripts/on-version.js
+++ scripts/on-version.js
@@ -5,3 +5,6 @@ const shell = require('shelljs');
 const version = require('../package.json').version;
 
 shell.sed('-i', 'VERSION \= \"[0-9\.]*\"', `VERSION = "${version}"`, 'defs.bzl');
+shell.sed(
+    '-i', '\/releases\/download\/[0-9\.]*\/rules_nodejs-[0-9\.]*.tar.gz',
+    `/releases/download/${version}/rules_nodejs-${version}.tar.gz`, 'README.md');
