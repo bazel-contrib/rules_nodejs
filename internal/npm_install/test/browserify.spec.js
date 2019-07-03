ed440be752389489f57993a2c1eb9685eef938f2
Alex Eagle <alexeagle@google.com>
2019-07-02 17:26:35 -0700

    test(builtin): add browserify test to repro #771

--- internal/npm_install/test/browserify.spec.js
+++ internal/npm_install/test/browserify.spec.js
@@ -6,11 +6,15 @@ describe('our bundled, vendored browserify binary', () => {
         const idxFile = fs.readFileSync(require.resolve(mainFile), {encoding: 'utf-8'});
         expect(idxFile).toContain('Copyright Joyent');
         expect(idxFile).toMatch(/Copyright \d+ Mozilla/);
-        expect(idxFile).toContain('Feross Aboukhadijeh');
+        expect(idxFile).toContain('The Dojo Foundation');
+    });
+    it('should have a named AMD module', () => {
+        const minimistUmd = require.resolve('build_bazel_rules_nodejs/internal/npm_install/test/minimist.umd.js');
+        expect(fs.readFileSync(minimistUmd, {encoding: 'utf-8'})).toContain(`define('minimist'`);
     });
     it('should work', () => {
-        const minimistUmd = require('build_bazel_rules_nodejs/internal/npm_install/test/some-package-name.umd.js');
-        const result = minimistUmd('h');
+        const minimist = require('build_bazel_rules_nodejs/internal/npm_install/test/minimist.umd.js');
+        const result = minimist('h');
         expect(result._[0]).toBe('h');
     });
 });
