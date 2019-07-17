5a87e046362d432e88a994a9e03f4165434ba00f
Alex Eagle <alexeagle@google.com>
2019-07-17 07:35:39 -0700

    fix(less): code review comments

--- packages/less/test/less_binary_spec.js
+++ packages/less/test/less_binary_spec.js
@@ -1,6 +1,6 @@
 const cssPath = require.resolve('npm_bazel_less/test/foo.css');
 const content = require('fs').readFileSync(cssPath, {encoding: 'utf-8'});
-if (content.indexOf('.link {\n  color: #428bca') < 0) {
+if (!content.includes('.link {\n  color: #428bca')) {
   console.error('Expected the css file to be transformed');
   process.exitCode = 1;
 }
