a47cd12eaa681abd22f7a5321bf15942b256f3ba
Alex Eagle <alexeagle@angular.io>
2019-07-24 13:14:32 -0700

    refactor(stylus): use jasmine_node_test
    Add compress and sourcemap as user-controlled options
    Address some code review comments

--- packages/stylus/test/stylus_binary_spec.js
+++ packages/stylus/test/stylus_binary_spec.js
@@ -1,25 +1,21 @@
-console.log(process.cwd());
 const cssPath = require.resolve('build_bazel_rules_nodejs/packages/stylus/test/file.css');
 const content = require('fs').readFileSync(cssPath, {encoding: 'utf-8'});
-if (content.indexOf('body #logo') < 0) {
-  console.error('Expected the css file to be transformed');
-  process.exitCode = 1;
-}
-if (content.indexOf('width:10px') < 0) {
-  console.error('Expected the css file to be transformed');
-  process.exitCode = 1;
-}
-if (content.match(/\r|\n/)) {
-  console.error('expected compressed but contained newlines', content);
-  process.exitCode = 1;
-}
-// the four ../ segments here are from bazel-out/[arch]/bin/test/ back to the workspace root
-// users will probably have a final packaging where the original png file is layed out
-// next to the css file, so this seems undesirable...
-if (content.indexOf('url("../../../../test/subdir/baz.png")') < 0) {
-  console.error(
-      'expected relative url to be resolved, see ' +
-      'http://stylus-lang.com/docs/executable.html#resolving-relative-urls-inside-imports' +
-      '\n' + content);
-  process.exitCode = 1;
-}
+
+describe('stylus_binary rule', () => {
+  it('should transform the css file', () => {
+    expect(content).toContain('body #logo');
+    expect(content).toContain('width:10px');
+  });
+  it('should compress the output', () => {
+    // compress option should remove newlines
+    expect(content).not.toMatch(/\r|\n/);
+  });
+  // see http://stylus-lang.com/docs/executable.html#resolving-relative-urls-inside-imports
+  it('should resolve relative URLs', () => {
+    // the ../ segments here are from bazel-out/[arch]/bin/packages/stylus/test back to the
+    // workspace root
+    // users will probably have a final packaging where the original png file is layed out
+    // next to the css file, so this seems undesirable...
+    expect(content).toContain('url("../../../../../../packages/stylus/test/subdir/baz.png")');
+  });
+});
