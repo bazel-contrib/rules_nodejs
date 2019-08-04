38583ca4ea75a3c529e501d328e61246a725babd
Alex Eagle <alexeagle@google.com>
2019-08-04 11:40:19 -0700

    build: wWIP more less stuff

---
+++ e2e/less/test.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+process.chdir(require('path').join(process.env['TEST_SRCDIR'], 'e2e_less'));
+console.error(fs.readdirSync('.'))
+const expected = fs.readFileSync('golden.css', 'utf-8');
+const actual = fs.readFileSync('test.css', 'utf-8');
+if (expected !== actual) {
+  console.error(`FAILED. Expected\n${expected}\n but was\n${actual}`);
+  process.exitCode = 1;
+}
