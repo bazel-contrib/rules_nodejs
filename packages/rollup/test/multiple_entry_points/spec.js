14003a0a0f65eb9a596e8e168d6e6b3b41997d6f
Alex Eagle <alexeagle@angular.io>
2019-09-10 12:20:55 -0700

    refactor(rollup): don't produce both directory output and files in that directory
    Bazel team says this is unsupported, and it doesn't work with RBE

--- packages/rollup/test/multiple_entry_points/spec.js
+++ packages/rollup/test/multiple_entry_points/spec.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 describe('rollup multiple entry points', () => {
   it('should produce a chunk for each entry point', () => {
-    expect(fs.existsSync(require.resolve(__dirname + '/bundle/one.js'))).toBeTruthy();
-    expect(fs.existsSync(require.resolve(__dirname + '/bundle/two.js'))).toBeTruthy();
+    expect(fs.existsSync(require.resolve(__dirname + '/chunks/one.js'))).toBeTruthy();
+    expect(fs.existsSync(require.resolve(__dirname + '/chunks/two.js'))).toBeTruthy();
   });
 });
