d846f453b7c7d15cf556eacb6a3cf43f1b98a852
Alex Eagle <alexeagle@angular.io>
2019-09-19 14:32:47 -0700

    cleanups (#1150)
    * chore: remove e2e/stylus and e2e/less
    
    These no longer use our bazel rules so they can just be demonstrated in the examples/app
    
    * chore: cleanup examples/angular
    
    some bits aren't needed since its now in our monorepo
    In particular the husky library here was installing a wrong path into the .git/hooks

--- e2e/stylus/test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const expected = fs.readFileSync(require.resolve('e2e_stylus/golden.css'), 'utf-8');
-const actual = fs.readFileSync(require.resolve('e2e_stylus/test.css'), 'utf-8');
-if (expected !== actual) {
-  console.error(`FAILED. Expected\n${expected}\n but was\n${actual}`);
-  process.exitCode = 1;
-}
