307a796eacde97303a3ef8e4751f9ea443c46877
Alex Eagle <alexeagle@angular.io>
2019-09-23 09:42:25 -0700

    feat(builtin): linker should resolve workspace-absolute paths

--- internal/linker/test/integration/program.js
+++ internal/linker/test/integration/program.js
@@ -4,8 +4,11 @@ const a = require('static_linked');
 // First-party package from ./dynamic_linked_pkg
 // it should get resolved from the execroot
 const b = require('dynamic_linked');
+// We've always supported `require('my_workspace')` for absolute imports like Google does it
+const c = require('build_bazel_rules_nodejs/internal/linker/test/integration/absolute_import');
+
 // Third-party package installed in the root node_modules
 const semver = require('semver');
 
 // This output should match what's in the golden.txt file
-console.log(a.addA(b.addB(semver.clean(' =v1.2.3 '))));
+console.log(a.addA(b.addB(c.addC(semver.clean(' =v1.2.3 ')))));
