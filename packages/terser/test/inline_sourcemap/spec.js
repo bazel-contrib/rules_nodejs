dc5f6b63f7e127d3a06035db7673deb68fb17cd4
Alex Eagle <alexeagle@angular.io>
2019-09-25 10:28:50 -0700

    refactor: runfiles.resolve helper

--- packages/terser/test/inline_sourcemap/spec.js
+++ packages/terser/test/inline_sourcemap/spec.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const sm = require('source-map');
+const {runfiles} = require('build_bazel_rules_nodejs/internal/linker');
+const pkg = 'packages/terser/test/inline_sourcemap';
 
 describe('terser sourcemap handling', () => {
   it('should produce a sourcemap output', async () => {
-    const file = require.resolve(__dirname + '/out.min.js.map');
+    const file = runfiles.resolve(process.env['TEST_WORKSPACE'], pkg, 'out.min.js.map');
     const rawSourceMap = JSON.parse(fs.readFileSync(file, 'utf-8'));
     await sm.SourceMapConsumer.with(rawSourceMap, null, consumer => {
       // terser will produce different output if the DEBUG environment variable is set
