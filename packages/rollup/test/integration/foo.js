7e980890fbe93de42cbffd5662b0961fb59f5d04
Greg Magolan <gmagolan@gmail.com>
2019-10-17 13:44:19 -0700

    fix(builtin): allow more than 2 segments in linker module names
    The linking code is made more robust to handle more than two segments in the module names. There is no technical reason to not allow this although it does diverge from the npm package standard of either foo or @scope/foo names.
    
    Module names are sorted and grouped to ensure that nested modules such as ‘c’ are symlinked first before ‘c/c’, ‘c/c/c/c’, etc and that groups of nested modules are symlinked in order while maintaining the parallelized symlink code for module names that are not nested. Gaps in the nested paths are filled with mkdir as needed and common paths between groups are mkdir'd before handing off to the parallelized symlink code.

--- packages/rollup/test/integration/foo.js
+++ packages/rollup/test/integration/foo.js
@@ -1,4 +1,8 @@
-import {foo} from 'foolib';
+import far_a from '@far/a';
+import far_a_b_c from '@far/a/b/c';
+import {foo} from '@foo/lib';
+import {foo as foo_a} from '@foo/lib/a';
+import {foo as foo_a_a_a} from '@foo/lib/a/a/a';
 import {fum} from 'fumlib';
 import hello from 'hello';
 import {thing} from 'some_global_var';
@@ -6,7 +10,8 @@ import {thing} from 'some_global_var';
 import {name} from './bar';
 import {json_key} from './some.json';
 
-console.log(`${hello}, ${name} in ${fum} ${foo} ${json_key}`);
+console.log(
+    `${hello}, ${name} in ${fum} ${foo} ${foo_a} ${foo_a_a_a} ${far_a} ${far_a_b_c} ${json_key}`);
 
 // Tests for @PURE annotations
 /*@__PURE__*/
