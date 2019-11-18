c7dd07908d9e91d56e1a9bdd7d1e0f4c40977983
Alex Eagle <alexeagle@google.com>
2019-11-18 14:51:05 -0800

    chore: move devserver/injector_spec to test folder

--- packages/typescript/test/devserver/injector_spec.js
+++ packages/typescript/test/devserver/injector_spec.js
@@ -1,4 +1,4 @@
-const injector = require('./injector');
+const injector = require('npm_bazel_typescript/internal/devserver/injector');
 
 describe('HTML injector', () => {
   const inFile = 'some/index.html';
