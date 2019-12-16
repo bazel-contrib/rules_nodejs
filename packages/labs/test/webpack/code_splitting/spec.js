d7287283ccafd249a1b1063c0c2fdf0692e25f1d
ray <rayman1104@gmail.com>
2019-12-16 16:40:25 +0300

    test(labs): add test for webpcak_bundle (code_splitting)

---
+++ packages/labs/test/webpack/code_splitting/spec.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const { runfiles } = require('build_bazel_rules_nodejs/internal/linker');
+
+describe('webpack code splitting', () => {
+  it('should produce a chunk for lazy loaded code', () => {
+    const chunks = runfiles.resolvePackageRelative('bundle');
+
+    expect(fs.existsSync(chunks + '/bundle.js')).toBeTruthy();
+    expect(fs.existsSync(chunks + '/chunk.js')).toBeTruthy();
+  });
+});
