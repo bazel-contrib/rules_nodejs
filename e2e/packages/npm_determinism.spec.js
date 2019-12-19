d245d09c15e76f8db2937f6f2db74863da7bde24
Greg Magolan <gmagolan@gmail.com>
2019-12-18 21:31:46 -0800

    refactor(builtin): add `args` to yarn_install & npm_install (#1462)
    BREAKING CHANGE:
    `args` in yarn_install and npm_install can be used to pass arbitrary arguments so we removed the following attributes:
    * prod_only from yarn_install and npm_install; should be replaced by args = ["--prod"] and args = ["--production"] respectively
    * frozen_lockfile from yarn_install; should be replaced by args = ["--frozen-lockfile"]
    * network_timeout from yanr_install; should be replaced by args = ["--network_timeout", "<time in ms>"]

--- e2e/packages/npm_determinism.spec.js
+++ e2e/packages/npm_determinism.spec.js
@@ -8,6 +8,13 @@ const packageJsonPath2 = packageJsonPath.replace(
     '/e2e_packages_npm_install_duplicate_for_determinism_testing/', '/e2e_packages_npm_install/');
 const packageJson2 = JSON.parse(fs.readFileSync(packageJsonPath2));
 
+try {
+  require.resolve('tmp');
+  console.error(
+      'expected tmp to not be installed by npm as --production was passed via args in npm_install');
+  process.exitCode = 1;
+} catch (_) {
+}
 
 if (packageJsonPath === packageJsonPath2) {
   console.error('expected different json paths');
