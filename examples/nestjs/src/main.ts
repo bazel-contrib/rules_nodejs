0495ce7bb07ad6f2423338162fc9a95c6181f301
Fabian Wiles <fabianwiles@live.com>
2019-12-30 18:31:37 +1300

    feat(examples): add nodejs_binary cluster example

--- examples/nestjs/src/main.ts
+++ examples/nestjs/src/main.ts
@@ -1,6 +1,8 @@
 import {INestApplication, Logger} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
 import {ExpressAdapter} from '@nestjs/platform-express';
+import * as cluster from 'cluster';
+import * as os from 'os';
 
 import {AppModule} from './app.module';
 
@@ -11,7 +13,26 @@ export async function bootstrap(port: number): Promise<INestApplication> {
   return app;
 }
 
+function main(port: number) {
+  if (cluster.isMaster) {
+    const cpuCount = os.cpus().length;
+
+    for (let i = 0; i < cpuCount; i += 1) {
+      cluster.fork();
+    }
+
+    cluster.on('online', worker => {
+      Logger.log('Worker ' + worker.process.pid + ' is online.');
+    });
+    cluster.on('exit', ({process}, code, signal) => {
+      Logger.log('worker ' + process.pid + ' died.');
+    });
+  } else {
+    bootstrap(port);
+  }
+}
+
 if (require.main === module) {
   const argv = require('minimist')(process.argv.slice(2));
-  bootstrap(argv.port || 3000);
+  main(argv.port || 3000);
 }
