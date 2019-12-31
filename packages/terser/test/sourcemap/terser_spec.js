7625a40424d183db6523947a5f9576d26ef32420
Greg Magolan <gmagolan@gmail.com>
2019-12-30 16:18:13 -0800

    test(terser): fix specs that check compilation_mode=dbg terser output
    These specs should not check COMPILATION_MODE since a test does not have a compilation_mode as all of the outputs have already been compiled. The specs should determine if the output was debug/non-debug based on the bazel-out folder name of the file being checked.

--- packages/terser/test/sourcemap/terser_spec.js
+++ packages/terser/test/sourcemap/terser_spec.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const sm = require('source-map');
 const DIR = 'build_bazel_rules_nodejs/packages/terser/test/sourcemap';
-const DEBUG = process.env['COMPILATION_MODE'] === 'dbg';
 
 describe('terser sourcemap handling', () => {
   it('should produce a sourcemap output', async () => {
     const file = require.resolve(DIR + '/src1.min.js.map');
+    const debugBuild = /\/bazel-out\/[^/\s]*-dbg\//.test(file);
     const rawSourceMap = JSON.parse(fs.readFileSync(file, 'utf-8'));
     await sm.SourceMapConsumer.with(rawSourceMap, null, consumer => {
       const pos = consumer.originalPositionFor(
           // position of MyClass in terser_minified output src1.min.js
           // depends on DEBUG flag
-          !DEBUG ? {line: 1, column: 18} : {line: 3, column: 5});
+          !debugBuild ? {line: 1, column: 18} : {line: 3, column: 5});
       expect(pos.source).toBe('src1.ts');
       expect(pos.line).toBe(2);
       expect(pos.column).toBe(14);
