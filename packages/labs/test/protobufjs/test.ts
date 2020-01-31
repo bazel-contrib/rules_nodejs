9a7c0d82c32dace2629c4c35cd811b1be1c51246
Cooper Bills <cbills@bostondynamics.com>
2020-01-31 11:33:10 -0800

    fix: remove stray comment

--- packages/labs/test/protobufjs/test.ts
+++ packages/labs/test/protobufjs/test.ts
@@ -1,7 +1,5 @@
 import {TestMessage} from 'build_bazel_rules_nodejs/packages/labs/test/protobufjs/test_ts_proto';
 
-// const TestMessage = build_bazel_rules_nodejs.packages.labs.test.protobufjs.TestMessage;
-
 describe('protobufjs', () => {
   it('should work in node', () => {
     expect(TestMessage.verify({
