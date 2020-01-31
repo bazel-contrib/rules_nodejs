4a473c8f2b2b74c822c74009b4fba54281fa68e7
Fabian Wiles <fabianwiles@live.com>
2020-01-31 07:36:58 +0000

    feat(examples): add protractor angular architect

--- examples/angular_bazel_architect/e2e/protractor.conf.js
+++ examples/angular_bazel_architect/e2e/protractor.conf.js
@@ -10,7 +10,6 @@ const {SpecReporter} = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: ['./src/**/*.e2e-spec.ts'],
-  capabilities: {'browserName': 'chrome'},
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
@@ -18,5 +17,14 @@ exports.config = {
   onPrepare() {
     require('ts-node').register({project: require('path').join(__dirname, './tsconfig.json')});
     jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+  },
+  capabilities: {
+    browserName: 'chrome',
+    pageLoadStrategy: 'normal',
+    chromeOptions: {
+      args: [
+        '--headless', '--disable-gpu', '--window-size=800,600', '--debuggerAddress=127.0.0.1:12633'
+      ]
+    }
   }
 };
