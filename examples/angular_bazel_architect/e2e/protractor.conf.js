d4200191c5fb84f395311840d8f90d3715e9f751
Fabian Wiles <fabianwiles@live.com>
2020-02-05 19:14:07 +1300

    feat(examples): add protractor angular architect (#1594)

--- examples/angular_bazel_architect/e2e/protractor.conf.js
+++ examples/angular_bazel_architect/e2e/protractor.conf.js
@@ -10,7 +10,6 @@ const {SpecReporter} = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: ['./src/**/*.e2e-spec.ts'],
-  capabilities: {'browserName': 'chrome'},
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
@@ -18,5 +17,27 @@ exports.config = {
   onPrepare() {
     require('ts-node').register({project: require('path').join(__dirname, './tsconfig.json')});
     jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      // `--no-sandbox` flag disables the chrome sandbox because it causes Chrome to crash on some
+      // environments
+      // http://chromedriver.chromium.org/help/chrome-doesn-t-start
+      // https://github.com/puppeteer/puppeteer/blob/v1.0.0/docs/troubleshooting.md#chrome-headless-fails-due-to-sandbox-issues
+      // `--headess` flag runs the browser in headless mode
+      // `--disable-gpu` flag disables GPU usage because it causes Chrome to crash on some
+      // environments
+      // `--disable-dev-shm-usage` flag disables the usage of `/dev/shm` because it causes Chrome to
+      // crash on some environments.
+      // https://github.com/puppeteer/puppeteer/blob/v1.0.0/docs/troubleshooting.md#tips
+      // https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t
+      // `--hide-scrollbars` flag comes from puppeteer headless mode defaults
+      // `--mute-audio` flag comes from puppeteer headless mode defaults
+      args: [
+        '--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage',
+        '--hide-scrollbars', '--mute-audio'
+      ]
+    }
   }
 };
