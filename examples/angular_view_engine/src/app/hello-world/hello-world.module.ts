a3b9768ee8e85c056528225f621343a2fbbe8db6
Jason Bedard <jason+github@jbedard.ca>
2020-03-30 16:06:08 -0700

    docs(examples): add use of ng_module(module_name) in angular example

--- examples/angular_view_engine/src/app/hello-world/hello-world.module.ts
+++ examples/angular_view_engine/src/app/hello-world/hello-world.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
+import {BigWordModule} from '@examples/angular/typography';
 import {MaterialModule} from '../../shared/material/material.module';
 
 import {HelloWorldComponent} from './hello-world.component';
@@ -9,7 +10,7 @@ import {HelloWorldComponent} from './hello-world.component';
   declarations: [HelloWorldComponent],
   imports: [
     FormsModule, RouterModule, MaterialModule,
-    RouterModule.forChild([{path: '', component: HelloWorldComponent}])
+    RouterModule.forChild([{path: '', component: HelloWorldComponent}]), BigWordModule
   ],
   exports: [HelloWorldComponent],
 })
