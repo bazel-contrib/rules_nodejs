97a4276291d357ded922c802daadcc5d04873974
Greg Magolan <gmagolan@gmail.com>
2020-04-02 15:58:38 -0700

    test: test that external repo script such as 'npm_bazel_jasmine/jasmine_runner.js' can require linked packages
    This commit turns on the linker & turn off require patches in a jasmine_node_test so that the external workspace jasmine_node_test entry point npm_bazel_jasmine/jasmine_runner.js's require('@bazel/jasmine') is exercised without require patches.

--- internal/node/test/npm_package_bin.spec.js
+++ internal/node/test/npm_package_bin.spec.js
@@ -2,14 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']);
 
-const min_js = path.join(runfiles.resolvePackageRelative('minified.js'));
-const content = fs.readFileSync(min_js, 'utf-8');
-if (!content.includes('{console.error("thing")}')) {
-  console.error(content);
-  process.exitCode = 1;
-}
+describe('npm_package_bin', function() {
+  it('should output a minified.js when output_dir is False', function() {
+    const content = fs.readFileSync(runfiles.resolvePackageRelative('minified.js'), 'utf-8');
+    expect(content).toContain('{console.error("thing")}')
+  });
 
-const dir = fs.readdirSync(path.join(path.dirname(min_js), 'dir_output'));
-if (!dir.includes('terser_input.js')) {
-  console.error(dir), process.exitCode = 1;
-}
+  it('should output a directory artifact named after the target when output_dir is True',
+     function() {
+       const dir = fs.readdirSync(runfiles.resolvePackageRelative('dir_output'));
+       expect(dir).toContain('terser_input.js');
+     });
+});
