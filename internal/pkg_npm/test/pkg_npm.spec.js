8e8993dcbb9dfb81ef8c498b0f2577d58aa58769
Greg Magolan <gmagolan@gmail.com>
2020-04-03 21:04:26 -0700

    fix(builtin): fix for pkg_npm single directory artifact dep case
    Also fixes the dependency on bazel-bin for the .pack & .publish scripts.
    
    This fixes an issue where pkg_npm has a single dep which is a directory artifact but the contents of that dep are not re-rooted. This is the expected behaviour as the only way to create a valid npm package with a package.json at the root is to re-root to the output directory of the dep. The fix is actually to not copy anything but to just forward the directory artifact dep onto the default info of pkg_npm. Some refactoring done so that the npm scripts are generated in a separate action from the package.js which is not run in the single directory artifact dep case.

--- internal/pkg_npm/test/pkg_npm.spec.js
+++ internal/pkg_npm/test/pkg_npm.spec.js
@@ -1,50 +1,62 @@
 const fs = require('fs');
-const path = require('path');
 const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']);
+const args = process.argv.slice(2);
+const testPkgRootpath = args[0];
+const testNoopPkgRootpath = args[1];
+const testNoop2PkgRootpath = args[2];
+const testPkgPath = runfiles.resolveWorkspaceRelative(testPkgRootpath);
+const testNoopPkgPath = runfiles.resolveWorkspaceRelative(testNoopPkgRootpath);
+const testNoop2PkgPath = runfiles.resolveWorkspaceRelative(testNoop2PkgRootpath);
 
-function read(p) {
-  // We want to look up the test_pkg directory artifact in the runfiles.
-  // The manifest does have an entry for it, but since it's a directory we cannot use
-  // require.resolve to lookup that entry. So instead we lookup the sibling file in runfiles and
-  // bootstrap the filesystem lookup from there.
-  const dir = path.dirname(runfiles.resolvePackageRelative('test_loader.js'));
-  return fs.readFileSync(path.join(dir, 'test_pkg', p), {encoding: 'utf-8'}).trim();
+function readFromPkg(p) {
+  return fs.readFileSync(`${testPkgPath}/${p}`, {encoding: 'utf-8'}).trim();
 }
 
-describe('pkg_npm srcs', () => {
+function readFromNoopPkg(p) {
+  return fs.readFileSync(`${testNoopPkgPath}/${p}`, {encoding: 'utf-8'}).trim();
+}
+
+function readFromNoop2Pkg(p) {
+  return fs.readFileSync(`${testNoop2PkgPath}/${p}`, {encoding: 'utf-8'}).trim();
+}
+
+describe('pkg_npm', () => {
+  it('creates an output directory after the rule name', () => {
+    expect(testPkgRootpath).toEqual('internal/pkg_npm/test/test_pkg');
+  });
   it('copies srcs and replaces contents', () => {
-    expect(read('some_file')).toEqual('replaced');
+    expect(readFromPkg('some_file')).toEqual('replaced');
   });
   it('copies dependencies from bazel-genfiles', () => {
-    expect(read('a_dep')).toEqual('a_dep content');
+    expect(readFromPkg('a_dep')).toEqual('a_dep content');
   });
   it('copies files from other packages', () => {
-    expect(read('dependent_file')).toEqual('dependent_file content');
+    expect(readFromPkg('dependent_file')).toEqual('dependent_file content');
   });
   it('copies js files from ts_library', () => {
-    expect(read('foo.js')).toContain('exports.a = \'\';');
+    expect(readFromPkg('foo.js')).toContain('exports.a = \'\';');
   });
   it('copies declaration files from ts_library', () => {
-    expect(read('foo.d.ts')).toContain('export declare const a: string;');
+    expect(readFromPkg('foo.d.ts')).toContain('export declare const a: string;');
   });
   it('copies data dependencies', () => {
-    expect(read('data.json')).toEqual('[]');
+    expect(readFromPkg('data.json')).toEqual('[]');
   });
   it('replaced 0.0.0-PLACEHOLDER', () => {
-    expect(JSON.parse(read('package.json')).version).toEqual('1.2.3');
+    expect(JSON.parse(readFromPkg('package.json')).version).toEqual('1.2.3');
   });
   it('copies files from deps', () => {
-    expect(read('bundle.min.js')).toBe('bundle content');
+    expect(readFromPkg('bundle.min.js')).toBe('bundle content');
   });
   it('copies files from external workspace if included in srcs', () => {
-    expect(read('vendored_external_file')).toEqual('vendored_external_file content');
+    expect(readFromPkg('vendored_external_file')).toEqual('vendored_external_file content');
   });
   it('copies js files from external workspace ts_library if included in vendor_external', () => {
-    expect(read('external.js')).toContain('exports.b = \'\';');
+    expect(readFromPkg('external.js')).toContain('exports.b = \'\';');
   });
   it('copies declaration files from external workspace ts_library if included in vendor_external',
      () => {
-       expect(read('external.d.ts')).toContain('export declare const b: string;');
+       expect(readFromPkg('external.d.ts')).toContain('export declare const b: string;');
      });
   it('vendors external workspaces',
      () => {
@@ -52,6 +64,16 @@ describe('pkg_npm srcs', () => {
          // has to live in the root of the repository in order for external/foo to appear inside it
      });
   it('copies entire contents of directories',
-     () => {expect(read('rollup/bundle/subdirectory/index.js'))
+     () => {expect(readFromPkg('rollup/bundle/subdirectory/index.js'))
                 .toContain(`const a = '';\n\nexport { a }`)});
+  it('does not create an output directory if single dep is a directory artifact', () => {
+    expect(testNoopPkgRootpath).toEqual('internal/pkg_npm/test/rollup/bundle/subdirectory');
+  });
+  it('re-roots to directory artifact if single dep is a directory artifact',
+     () => {expect(readFromNoopPkg('index.js')).toContain(`const a = '';\n\nexport { a }`)});
+  it('does not create an output directory if single dep is a directory artifact', () => {
+    expect(testNoop2PkgRootpath).toEqual('internal/pkg_npm/test/rollup/bundle/subdirectory');
+  });
+  it('re-roots to directory artifact if single dep is a directory artifact',
+     () => {expect(readFromNoop2Pkg('index.js')).toContain(`const a = '';\n\nexport { a }`)});
 });
