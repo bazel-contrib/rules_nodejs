17c8ad8764ae4e5acb2e08024b26109dcf82f6fd
flolu <fllu@pm.me>
2020-04-25 13:29:34 +0200

    feat(example): service worker update handling

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import {Component} from '@angular/core';
 
+import { ServiceWorkerService } from './service-worker.service';
+
 @Component({selector: 'app-component', templateUrl: 'app.component.html'})
 export class AppComponent {
+  constructor(private swService: ServiceWorkerService) {
+    this.swService.launchUpdateCheckingRoutine()
+  }
 }
