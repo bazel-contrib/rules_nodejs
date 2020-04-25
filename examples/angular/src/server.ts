4d5b9c7e91e55f05dbd23fcc50b52594d0495321
flolu <fllu@pm.me>
2020-04-25 13:08:10 +0200

    feat(example): add full pwa support

--- examples/angular/src/server.ts
+++ examples/angular/src/server.ts
@@ -1,41 +1,28 @@
-///<reference types="node"/>
-import "zone.js/dist/zone-node";
+///<reference types='node'/>
+import 'zone.js/dist/zone-node';
 
-import { ngExpressEngine } from "@nguniversal/express-engine";
-import * as express from "express";
-import { join } from "path";
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import * as express from 'express';
+import { join } from 'path';
+import * as compression from 'compression';
+
+import { AppServerModule } from './app/app.server.module';
 
 const app = express();
+const port = process.env.PORT || 4000;
+const DIST_FOLDER = join(process.cwd(), 'src/pwa');
 
-const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = join(process.cwd(), "src/prodapp");
+/**
+ * text compression for smaller download sizes and thus faster load times
+ * without compression: 1.4 MB
+ * with compresion: 321 kB
+ */
+app.use(compression());
 
-import { AppServerModule } from "./app/app.server.module";
+app.engine('html', ngExpressEngine({ bootstrap: AppServerModule }) as any);
+app.set('view engine', 'html');
+app.set('views', DIST_FOLDER);
 
-app.engine(
-  "html",
-  ngExpressEngine({
-    bootstrap: AppServerModule,
-    providers: [
-      // TODO add support for lazy loading with server side rendering
-      // provideModuleMap(LAZY_MODULE_MAP)
-    ]
-  }) as any
-);
-
-app.set("view engine", "html");
-app.set("views", DIST_FOLDER);
-
-app.get("*.*", express.static(DIST_FOLDER, { maxAge: "1y" }));
-
-// catch /favicon.ico route to prevent the following server error:
-// Error: Cannot match any routes. URL Segment: 'favicon.ico'
-app.get("/favicon.ico", (req, res) => res.send(""));
-
-app.get("*", (req, res) => {
-  res.render("example/index", { req });
-});
-
-app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
-});
+app.get('*.*', express.static(DIST_FOLDER, { maxAge: '1y' }));
+app.get('*', (req, res) => res.render('example/index', { req }));
+app.listen(port, () => console.log(`Server listening http://localhost:${port}`));
