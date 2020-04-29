0407d2552b00781b60dba65ab434e8ff98b756b4
Greg Magolan <gmagolan@gmail.com>
2020-04-29 13:48:04 -0700

    test: copy_to_bin shenanigans for Windows CI

--- internal/linker/test/issue_1823_use_ts_library_esm/jest.config.js
+++ internal/linker/test/issue_1823_use_ts_library_esm/jest.config.js
@@ -1,6 +1,18 @@
+const isWindows = process.platform === 'win32';
+
 module.exports = {
   testEnvironment: 'node',
   transform: {'^.+\\.mjs?$': 'babel-jest'},
-  testMatch: ['**/?(*.)(spec|test).?(m)js?(x)'],
+  // More Shenanigans for Windows where jest is running both the original .mjs & the babel
+  // transformed .js and fails. Not sure what why this is the case or exactly why changing
+  // testMatch solves it but not a priority to solve:
+  // ```
+  // PASS ../../lib.test.js
+  // FAIL ../../lib.test.mjs
+  // C:\b\swkzcapm\execroot\build_bazel_rules_nodejs\bazel-out\x64_windows-fastbuild\bin\internal\linker\test\issue_1823_use_ts_library_esm\lib.test.mjs:1
+  //     import { doStuff } from './lib';
+  //     ^^^^^^
+  // ```
+  testMatch: [isWindows ? '**/?(*.)(spec|test).js?(x)' : '**/?(*.)(spec|test).?(m)js?(x)'],
   moduleFileExtensions: ['js', 'mjs'],
 };
