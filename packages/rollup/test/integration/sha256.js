6247a7afb89a70a17c3db335e535fa265af8457b
Alex Eagle <alex.eagle@robinhood.com>
2020-06-14 10:22:18 -0700

    test(rollup): remove unstable sha256 tests, use regular golden

--- packages/rollup/test/integration/sha256.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-const fs = require('fs');
-const sha256sum = crypto.createHash('sha256');
-sha256sum.update(fs.readFileSync(process.argv[2], {encoding: 'utf-8'}));
-fs.writeFileSync(process.argv[3], sha256sum.digest('hex'));
