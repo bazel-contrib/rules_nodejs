4d941e627de00e16ed2cd26a250041fff9cdce18
Alex Eagle <alex.eagle@robinhood.com>
2020-07-05 13:04:03 -0700

    refactor: simplify json output file declaration
    Never predeclare these outputs, since in most cases we can't

--- packages/typescript/test/ts_project/json/verify.js
+++ packages/typescript/test/ts_project/json/verify.js
@@ -1,18 +1,8 @@
-const files = process.argv.slice(2)
-let barJson
-let subdirFooJson
-files.forEach(f => {
-  if (f.endsWith('bar.json')) {
-    barJson = f
-  } else if (f.endsWith('subdir/foo.json')) {
-    subdirFooJson = f
-  }
-})
-if (!barJson) {
-  console.error('Missing bar.json')
-  process.exitCode = 1
-}
-else if (!subdirFooJson) {
-  console.error('Missing subdir/foo.json')
-  process.exitCode = 1
-}
+const assert = require('assert');
+
+const files = process.argv.slice(2);
+assert.ok(files.some(f => f.endsWith('json/bar.json')), 'Missing bar.json');
+assert.ok(files.some(f => f.endsWith('json/subdir/foo.json')), 'Missing subdir/foo.json');
+assert.ok(files.some(f => f.endsWith('json/foobar/bar.json')), 'Missing outdir bar.json');
+assert.ok(
+    files.some(f => f.endsWith('json/foobar/subdir/foo.json')), 'Missing outdir subdir/foo.json');
