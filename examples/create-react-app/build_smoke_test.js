2e334b56cbb0a9f67e6ccb39670388145efc16ce
Alex Eagle <alex.eagle@robinhood.com>
2020-07-10 16:51:51 -0700

    ci: add testing for create-react-app example

---
+++ examples/create-react-app/build_smoke_test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+const fs = require('fs');
+const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']);
+
+// Make sure there's a file like build/static/js/main.12345678.chunk.js
+const jsDir = runfiles.resolvePackageRelative('build/static/js');
+assert.ok(fs.readdirSync(jsDir).some(f => /main\.[0-9a-f]{8}\.chunk\.js/.test(f)));
