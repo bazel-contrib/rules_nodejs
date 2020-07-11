4e53157816282a8d29cae609f6a4ef493efacf61
Long Ho <holevietlong@gmail.com>
2020-07-11 16:11:25 -0400

    docs(typescript): add jest snapshot update rule

--- examples/jest/index.test.js
+++ examples/jest/index.test.js
@@ -3,3 +3,7 @@ import indexfile from './index.js';
 test('it should work', () => {
   expect(indexfile).toBe('test');
 });
+
+test('snapshot', function() {
+  expect(indexfile).toMatchSnapshot();
+})
