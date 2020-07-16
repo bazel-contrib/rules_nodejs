75f7a61a660c012a187bf38241463e0e2b814dac
Greg Magolan <greg.magolan@robinhood.com>
2020-07-16 08:34:32 -0700

    test: update e2e/nodejs_image test to include 1st party & 3rd party npm deps to link

--- e2e/nodejs_image/main.js
+++ e2e/nodejs_image/main.js
@@ -1,1 +1,5 @@
-console.log('hello world');
+const format = require('date-fns').format;
+const foo = require('@foo/lib').foo;
+
+const date = format(new Date(2019, 4, 7), 'MMMM d, yyyy');
+console.log(foo('lib'), date);
