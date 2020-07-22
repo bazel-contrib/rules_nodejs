ee9367bab719ba264976b667a73b8dd7da9dd780
Alex Eagle <alex.eagle@robinhood.com>
2020-07-22 15:14:58 -0700

    feat(examples): initial checkin of next.js example
    Ran npm init next-app nextjs-blog --example https://github.com/vercel/next-learn-starter/tree/master/data-fetching-starter

---
+++ examples/nextjs-blog/pages/_app.js
@@ -0,0 +1,6 @@
+/* clang-format off */
+import '../styles/global.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ examples/nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,20 @@
+/* clang-format off */
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
+}
