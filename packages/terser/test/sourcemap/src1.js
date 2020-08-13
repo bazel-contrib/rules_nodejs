ba7deb315eaae4a1a1be76033fb4070374c1f962
Tiago Costa <tiagoffcc@hotmail.com>
2020-08-13 04:11:05 +0100

    fix(builtin): wrong import of copy_to_bin bzl file. Now importing from index.

--- packages/terser/test/sourcemap/src1.js
+++ packages/terser/test/sourcemap/src1.js
@@ -1,10 +1,6 @@
-exports.__esModule = true;
 // clang-format off
-/*a comment*/ var MyClass = /** @class */ (function () {
-    function MyClass(s) {
+/*a comment*/ export class MyClass {
+    constructor(s) {
         console.log(s);
     }
-    return MyClass;
-}());
-exports.MyClass = MyClass;
-//# sourceMappingURL=src1.js.map
+}
