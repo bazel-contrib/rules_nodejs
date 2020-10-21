da87a2cf0fbdd33833867ae4c552f7cc08bc035f
Fredrik Bonander <carl.fredrik.bonander@gmail.com>
2020-10-21 07:38:39 +0200

    fix: adds missing dependency

--- examples/jest/ts/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',
-    reporters: ['default'],
-    testMatch: ['ts/**/*.test.js'],
-    moduleNameMapper: {
-        'examples_jest/(.*)': '<rootDir>/$1',
-    },
-};
