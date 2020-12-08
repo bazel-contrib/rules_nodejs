f70ae45b51f10f6582edc56c762d46d44d5a6aab
Alex Eagle <alex.eagle@robinhood.com>
2020-12-08 11:26:45 -0800

    chore: next batch of bazel_patch_module_resolver fixes

--- e2e/webapp/test.js
+++ e2e/webapp/test.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const content = fs.readFileSync(require.resolve('e2e_webapp/out.min/app.js'), 'utf-8');
+const runfiles = require(process.env['BAZEL_NODE_RUNFILES_HELPER']);
+const content = fs.readFileSync(runfiles.resolve('e2e_webapp/out.min/app.js'), 'utf-8');
 if (content.indexOf('import("./strings') < 0) {
   console.error(content);
   process.exitCode = 1;
