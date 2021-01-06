266cb7326a6278ab3a633cad48305e90b936975c
Matthew Simmons <simmonmt@acm.org>
2021-01-06 13:04:24 -0500

    Create a react_webpack example that uses ts_library

---
+++ examples/react_webpack_tslibrary/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as styles from "./styles.css";
+
+ReactDOM.render(
+  <h1 className={styles.h1}>Hello, world!</h1>,
+  document.getElementById("root")
+);
