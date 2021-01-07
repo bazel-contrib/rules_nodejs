62b4c8634fb4c53e7dff6909893c77808da69b0c
Greg Magolan <greg.magolan@robinhood.com>
2021-01-06 16:16:12 -0800

    chore: show how to specify the path to babel config in the jest example

--- examples/jest/jest.config.js
+++ examples/jest/jest.config.js
@@ -1,6 +1,11 @@
+const path = require('path');
+
 module.exports = {
   testEnvironment: 'node',
   reporters: ['default', './jest-reporter'],
-  transform: {'^.+\\.jsx?$': 'babel-jest'},
+  // explicitly specify the path to babel.config.js relative to jest.config.js so
+  // jest can find it even when jest.config.js is not in the root folder of the workspace
+  transform:
+      {'^.+\\.[jt]sx?$': ['babel-jest', {configFile: path.resolve(__dirname, 'babel.config.js')}]},
   testMatch: ['**/*.test.js']
 };
