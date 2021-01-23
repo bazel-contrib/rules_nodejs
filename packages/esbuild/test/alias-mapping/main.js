c087357062fa9d341e1071b67bd759d7887464d0
Matt Mackay <mattem@gmail.com>
2021-01-23 18:20:43 -0500

    feat: add esbuild package

---
+++ packages/esbuild/test/alias-mapping/main.js
@@ -0,0 +1,4 @@
+import {getId as m1Id} from '@alias-mapping/module-one';
+import {getId as m2Id} from '@alias-mapping/module-two';
+
+export default `Full ID: ${m1Id} - ${m2Id}`;
---
+++ packages/esbuild/test/splitting/other.ts
@@ -0,0 +1,1 @@
+export const OTHER = true;
