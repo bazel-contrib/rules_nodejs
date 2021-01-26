657357239d4bf07b8c90939ee5d79197da77314f
Greg Magolan <greg.magolan@robinhood.com>
2021-01-25 20:04:55 -0800

    fix: linker fix for invalid symlink creation path in createSymlinkAndPreserveContents

--- internal/linker/index.js
+++ internal/linker/index.js
@@ -99,9 +99,6 @@ function deleteDirectory(p) {
 function symlink(target, p) {
     return __awaiter(this, void 0, void 0, function* () {
         log_verbose(`symlink( ${p} -> ${target} )`);
-        if (!(yield exists(target))) {
-            return false;
-        }
         try {
             yield fs.promises.symlink(target, p, 'junction');
             return true;
@@ -455,7 +452,7 @@ function main(args, runfiles) {
                 yield mkdirp(path.dirname(m.name));
                 if (m.link) {
                     const [root, modulePath] = m.link;
-                    let target = '<package linking failed>';
+                    let target;
                     switch (root) {
                         case 'execroot':
                             if (isExecroot) {
@@ -487,17 +484,32 @@ function main(args, runfiles) {
                                     }
                                 }
                             }
-                            catch (_a) {
-                                target = '<runfiles resolution failed>';
+                            catch (err) {
+                                target = undefined;
+                                log_verbose(`runfiles resolve failed for module '${m.name}': ${err.message}`);
                             }
                             break;
                     }
                     const stats = yield gracefulLstat(m.name);
-                    if (stats !== null && (yield isLeftoverDirectoryFromLinker(stats, m.name))) {
-                        yield createSymlinkAndPreserveContents(stats, m.name, target);
+                    const isLeftOver = (stats !== null && (yield isLeftoverDirectoryFromLinker(stats, m.name)));
+                    if (target && (yield exists(target))) {
+                        if (stats !== null && isLeftOver) {
+                            yield createSymlinkAndPreserveContents(stats, m.name, target);
+                        }
+                        else {
+                            yield symlink(target, m.name);
+                        }
                     }
                     else {
-                        yield symlink(target, m.name);
+                        if (!target) {
+                            log_verbose(`no symlink target found for module ${m.name}`);
+                        }
+                        else {
+                            log_verbose(`potential target ${target} does not exists for module ${m.name}`);
+                        }
+                        if (isLeftOver) {
+                            yield unlink(m.name);
+                        }
                     }
                 }
                 if (m.children) {
