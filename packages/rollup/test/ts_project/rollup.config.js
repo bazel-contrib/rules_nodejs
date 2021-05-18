92b6aafdd91e56748262950d7975c2ff2a5ce3be
Jason Bedard <jason+github@jbedard.ca>
2021-05-17 22:02:17 -0700

    test(rollup): add ts_library, ts_project, alias-mapping tests

---
+++ packages/rollup/test/ts_project/rollup.config.js
@@ -0,0 +1,13 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+module.exports = {
+  onwarn: (warning) => {
+    // Always fail on warnings, assuming we don't know which are harmless.
+    // We can add exclusions here based on warning.code, if we discover some
+    // types of warning should always be ignored under bazel.
+    throw new Error(warning.message);
+  },
+  plugins: [
+    nodeResolve(),
+  ],
+};
