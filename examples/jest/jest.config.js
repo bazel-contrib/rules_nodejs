915a88b01c356e40585ba10eb26dcc9ec695978b
Yuchao <yuchao.dev@gmail.com>
2021-06-11 14:18:15 +0800

    chore: upgrade jest example to jest@17
    replace the patch with setting enableSymlinks to true. ref: #2338

--- examples/jest/jest.config.js
+++ examples/jest/jest.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   testEnvironment: 'node',
+  haste: {
+    enableSymlinks: true,
+  },
   reporters: ['default', './jest-reporter'],
   // explicitly specify the path to babel.config.js relative to jest.config.js so
   // jest can find it even when jest.config.js is not in the root folder of the workspace
