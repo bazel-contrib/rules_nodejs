c428c98ef9db4d75fc68193cccbe51980d544c28
Smokrow <moritz.kroeger@tu-dortmund.de>
2021-06-22 19:18:59 +0200

    cleaned up the file names and the vue.config.js

--- examples/vue/vue.config.js
+++ examples/vue/vue.config.js
@@ -1,11 +1,2 @@
 // vue.config.js
 css: { extract: false }
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      modules: [
-        "bazel-out/k8-fastbuild/bin",
-      ]
-    }
-  }
-}
