bazel-contrib/rules_nodejs

commit 2c2cc6eec42b0f4f805fb3063920107b78f821fd

feat: support for nested node_modules in linker
This PR adds support for nested node_modules folders in the linker, which allows targets to depend on npm packages from multiple yarn_install & npm_install repository rules (max one per directory in the workspace).

This is an opt-in feature which is enabled by specifying package_path in yarn_install or npm_install for nested package.json files. Setting package_path informs the linker to link the external npm repository to a node_modules folder in the package_path sub-directory specified.

For example, given two yarn_install rules:

```
yarn_install(
    name = "npm",
    package_json = "//:package.json",
    yarn_lock = "//:yarn.lock",
)

yarn_install(
    name = "npm_subdir",
    package_json = "//subdir:package.json",
    package_path = "subdir",
    yarn_lock = "//subdir:yarn.lock",
)
```

a target may depend on npm packages from both,

```
jasmine_node_test(
    name = "test",
    srcs = ["test.js"],
    deps = [
        "@npm//foo",
        "@npm_subdir//bar",
    ],
)
```

and the linker will link multiple 3rd party node_modules folders,

```
/node_modules => @npm//:node_modules
/subdir/node_modules => @npm_subdir/:node_modules
```

making the 3rd party npm deps foo & bar available at

```
/node_modules/foo
/subdir/node_modules/bar
```

This feature is opt-in as package_path currently defaults to "". In a future major release (4.0.0), package_path will default to the directory the package.json file is in, which will turn it on by default.

This feature is an alternative to yarn workspaces & npm workspaces, which also lay out node_modules in multiple directories from multiple package.json files.

internal/node/node.bzl
@@ -40,27 +40,19 @@ def _trim_package_node_modules(package_name):
         segments.append(n)
     return "/".join(segments)
 
-def _compute_node_modules_root(ctx):
-    """Computes the node_modules root from the deps attributes.
-
-    Args:
-      ctx: the skylark execution context
-
-    Returns:
-      The node_modules root as a string
-    """
-    node_modules_root = None
+def _compute_node_modules_roots(ctx):
+    """Computes the node_modules root (if any) from data attribute."""
+    node_modules_roots = {}
     for d in ctx.attr.data:
         if ExternalNpmPackageInfo in d:
-            possible_root = "/".join([d[ExternalNpmPackageInfo].workspace, "node_modules"])
-            if not node_modules_root:
-                node_modules_root = possible_root
-            elif node_modules_root != possible_root:
-                fail("All npm dependencies need to come from a single workspace. Found '%s' and '%s'." % (node_modules_root, possible_root))
-    if not node_modules_root:
-        # there are no fine grained deps but we still need a node_modules_root even if it is a non-existant one
-        node_modules_root = "build_bazel_rules_nodejs/node_modules"
-    return node_modules_root
+            path = d[ExternalNpmPackageInfo].path
+            workspace = d[ExternalNpmPackageInfo].workspace
+            if path in node_modules_roots:
+                other_workspace = node_modules_roots[path]
+                if other_workspace != workspace:
+                    fail("All npm dependencies at the path '%s' must come from a single workspace. Found '%s' and '%s'." % (path, other_workspace, workspace))
+            node_modules_roots[path] = workspace
+    return node_modules_roots
 
 def _write_require_patch_script(ctx, node_modules_root):
     # Generates the JavaScript snippet of module roots mappings, with each entry
@@ -167,9 +159,15 @@ def _nodejs_binary_impl(ctx):
             sources_depsets.append(d.files)
     sources = depset(transitive = sources_depsets)
 
-    node_modules_root = _compute_node_modules_root(ctx)
+    node_modules_roots = _compute_node_modules_roots(ctx)
 
+    if "" in node_modules_roots:
+        node_modules_root = node_modules_roots[""] + "/node_modules"
+    else:
+        # there are no fine grained deps but we still need a node_modules_root even if it is a non-existant one
+        node_modules_root = "build_bazel_rules_nodejs/node_modules"
     _write_require_patch_script(ctx, node_modules_root)
+
     _write_loader_script(ctx)
 
     # Provide the target name as an environment variable avaiable to all actions for the
@@ -184,12 +182,18 @@ def _nodejs_binary_impl(ctx):
     # runfiles helpers to use.
     env_vars += "export BAZEL_WORKSPACE=%s\n" % ctx.workspace_name
 
-    # if BAZEL_NODE_MODULES_ROOT has not already been set by
+    bazel_node_module_roots = ""
+    for path, root in node_modules_roots.items():
+        if bazel_node_module_roots:
+            bazel_node_module_roots = bazel_node_module_roots + ","
+        bazel_node_module_roots = bazel_node_module_roots + "%s:%s" % (path, root)
+
+    # if BAZEL_NODE_MODULES_ROOTS has not already been set by
     # run_node, then set it to the computed value
-    env_vars += """if [[ -z "${BAZEL_NODE_MODULES_ROOT:-}" ]]; then
-  export BAZEL_NODE_MODULES_ROOT=%s
+    env_vars += """if [[ -z "${BAZEL_NODE_MODULES_ROOTS:-}" ]]; then
+  export BAZEL_NODE_MODULES_ROOTS=%s
 fi
-""" % node_modules_root
+""" % bazel_node_module_roots
 
     for k in ctx.attr.configuration_env_vars + ctx.attr.default_env_vars:
         # Check ctx.var first & if env var not in there then check

internal/providers/external_npm_package_info.bzl
@@ -22,28 +22,41 @@ ExternalNpmPackageInfo = provider(
     doc = "Provides information about one or more external npm packages",
     fields = {
         "direct_sources": "Depset of direct source files in these external npm package(s)",
+        "path": "The local workspace path that these external npm deps should be linked at. If empty, they will be linked at the root.",
         "sources": "Depset of direct & transitive source files in these external npm package(s) and transitive dependencies",
         "workspace": "The workspace name that these external npm package(s) are provided from",
     },
 )
 
 def _node_modules_aspect_impl(target, ctx):
-    providers = []
+    if ExternalNpmPackageInfo in target:
+        return []
 
     # provide ExternalNpmPackageInfo if it is not already provided there are ExternalNpmPackageInfo deps
-    if not ExternalNpmPackageInfo in target:
-        sources_depsets = []
-        workspace = None
-        if hasattr(ctx.rule.attr, "deps"):
-            for dep in ctx.rule.attr.deps:
-                if ExternalNpmPackageInfo in dep:
-                    if workspace and dep[ExternalNpmPackageInfo].workspace != workspace:
-                        fail("All npm dependencies need to come from a single workspace. Found '%s' and '%s'." % (workspace, dep[ExternalNpmPackageInfo].workspace))
-                    workspace = dep[ExternalNpmPackageInfo].workspace
-                    sources_depsets.append(dep[ExternalNpmPackageInfo].sources)
-            if workspace:
-                providers.extend([ExternalNpmPackageInfo(direct_sources = depset(), sources = depset(transitive = sources_depsets), workspace = workspace)])
+    providers = []
 
+    # map of 'path' to [workspace, sources_depsets]
+    paths = {}
+    if hasattr(ctx.rule.attr, "deps"):
+        for dep in ctx.rule.attr.deps:
+            if ExternalNpmPackageInfo in dep:
+                path = dep[ExternalNpmPackageInfo].path
+                workspace = dep[ExternalNpmPackageInfo].workspace
+                sources_depsets = []
+                if path in paths:
+                    path_entry = paths[path]
+                    if path_entry[0] != workspace:
+                        fail("All npm dependencies at the path '%s' must come from a single workspace. Found '%s' and '%s'." % (path, workspace, path_entry[0]))
+                    sources_depsets = path_entry[1]
+                sources_depsets.append(dep[ExternalNpmPackageInfo].sources)
+                paths[path] = [workspace, sources_depsets]
+        for path, path_entry in paths.items():
+            providers.extend([ExternalNpmPackageInfo(
+                direct_sources = depset(),
+                sources = depset(transitive = path_entry[1]),
+                workspace = path_entry[0],
+                path = path,
+            )])
     return providers
 
 node_modules_aspect = aspect(

internal/providers/node_runtime_deps_info.bzl
@@ -39,9 +39,9 @@ do the same.
     },
 )
 
-def _compute_node_modules_root(ctx):
+def _compute_node_modules_roots(ctx):
     """Computes the node_modules root (if any) from data & deps targets."""
-    node_modules_root = ""
+    node_modules_roots = {}
     deps = []
     if hasattr(ctx.attr, "data"):
         deps += ctx.attr.data
@@ -49,12 +49,14 @@ def _compute_node_modules_root(ctx):
         deps += ctx.attr.deps
     for d in deps:
         if ExternalNpmPackageInfo in d:
-            possible_root = "/".join([d[ExternalNpmPackageInfo].workspace, "node_modules"])
-            if not node_modules_root:
-                node_modules_root = possible_root
-            elif node_modules_root != possible_root:
-                fail("All npm dependencies need to come from a single workspace. Found '%s' and '%s'." % (node_modules_root, possible_root))
-    return node_modules_root
+            path = d[ExternalNpmPackageInfo].path
+            workspace = d[ExternalNpmPackageInfo].workspace
+            if path in node_modules_roots:
+                other_workspace = node_modules_roots[path]
+                if other_workspace != workspace:
+                    fail("All npm dependencies at the path '%s' must come from a single workspace. Found '%s' and '%s'." % (path, other_workspace, workspace))
+            node_modules_roots[path] = workspace
+    return node_modules_roots
 
 def run_node(ctx, inputs, arguments, executable, chdir = None, **kwargs):
     """Helper to replace ctx.actions.run
@@ -127,7 +129,13 @@ def run_node(ctx, inputs, arguments, executable, chdir = None, **kwargs):
                 env[var] = ctx.var[var]
             elif var in ctx.configuration.default_shell_env.keys():
                 env[var] = ctx.configuration.default_shell_env[var]
-    env["BAZEL_NODE_MODULES_ROOT"] = _compute_node_modules_root(ctx)
+    bazel_node_module_roots = ""
+    node_modules_roots = _compute_node_modules_roots(ctx)
+    for path, root in node_modules_roots.items():
+        if bazel_node_module_roots:
+            bazel_node_module_roots = bazel_node_module_roots + ","
+        bazel_node_module_roots = bazel_node_module_roots + "%s:%s" % (path, root)
+    env["BAZEL_NODE_MODULES_ROOTS"] = bazel_node_module_roots
 
     # ctx.actions.run accepts both lists and a depset for inputs. Coerce the original inputs to a
     # depset if they're a list, so that extra inputs can be combined in a performant manner.

internal/linker/test/multi_linker/BUILD.bazel
@@ -1,0 +1,38 @@
+load("//packages/jasmine:index.bzl", "jasmine_node_test")
+load("//packages/typescript:checked_in_ts_project.bzl", "checked_in_ts_project")
+
+checked_in_ts_project(
+    name = "checked_in_from_sources_test",
+    src = "test.ts",
+    checked_in_js = "from_sources_test.js",
+    deps = [
+        "@npm//@types/jest",
+        "@npm//@types/node",
+    ],
+)
+
+# Test with a .js file in the output-tree to ensure that we can
+# resolve multi-linked node_modules from that context
+jasmine_node_test(
+    name = "test",
+    srcs = ["test.js"],
+    deps = [
+        "//internal/linker/test/multi_linker/onepa",
+        "//internal/linker/test/multi_linker/onepb",
+        "@npm//semver",
+        "@npm_internal_linker_test_multi_linker//semver",
+    ],
+)
+
+# Test with a .js file from the source tree to ensure that we can
+# resolve multi-linked node_modules from that context
+jasmine_node_test(
+    name = "from_sources_test",
+    srcs = ["from_sources_test.js"],
+    deps = [
+        "//internal/linker/test/multi_linker/onepa",
+        "//internal/linker/test/multi_linker/onepb",
+        "@npm//semver",
+        "@npm_internal_linker_test_multi_linker//semver",
+    ],
+)

internal/linker/test/multi_linker/onepa/BUILD.bazel
@@ -1,0 +1,14 @@
+load("//:index.bzl", "js_library")
+
+js_library(
+    name = "onepa",
+    package_name = "@rules_nodejs/onepa",
+    srcs = [
+        "index.js",
+        "package.json",
+    ],
+    visibility = ["//internal/linker/test/multi_linker:__pkg__"],
+    deps = [
+        "@onepa_npm_deps//:node_modules",
+    ],
+)

internal/linker/test/multi_linker/onepb/BUILD.bazel
@@ -1,0 +1,14 @@
+load("//:index.bzl", "js_library")
+
+# For negative testing, an alternative to 'onepb'
+# that doesn't include the transitive semver npm dep
+# that @onepa_npm_deps//:node_modules does
+js_library(
+    name = "onepb",
+    package_name = "@rules_nodejs/onepb",
+    srcs = [
+        "index.js",
+        "package.json",
+    ],
+    visibility = ["//internal/linker/test/multi_linker:__pkg__"],
+)

WORKSPACE
@@ -19,6 +19,7 @@ workspace(
         # cypress_deps must be a managed directory to ensure it is downloaded before cypress_repository is run.
         "@cypress_deps": ["packages/cypress/test/node_modules"],
         "@npm": ["node_modules"],
+        "@npm_internal_linker_test_multi_linker": ["internal/linker/test/multi_linker/node_modules"],
         "@npm_node_patches": ["packages/node-patches/node_modules"],
     },
 )
@@ -55,6 +56,21 @@ yarn_install(
     },
     package_json = "//:package.json",
     yarn_lock = "//:yarn.lock",
+)
+
+yarn_install(
+    name = "npm_internal_linker_test_multi_linker",
+    package_json = "//internal/linker/test/multi_linker:package.json",
+    package_path = "internal/linker/test/multi_linker",
+    yarn_lock = "//internal/linker/test/multi_linker:yarn.lock",
+)
+
+yarn_install(
+    name = "onepa_npm_deps",
+    package_json = "//internal/linker/test/multi_linker/onepa:package.json",
+    package_path = "internal/linker/test/multi_linker/onepa",
+    symlink_node_modules = False,
+    yarn_lock = "//internal/linker/test/multi_linker/onepa:yarn.lock",
 )
 
 npm_install(

internal/bazel_integration_test/test_runner.js
@@ -358,11 +358,10 @@ for (const bazelCommand of config.bazelCommands) {
     'BASH_FUNC_is_absolute%%',
     'BASH_FUNC_rlocation%%',
     'BASH_FUNC_runfiles_export_envvars%%',
-    'BAZEL_NODE_MODULES_ROOT',
+    'BAZEL_NODE_MODULES_ROOTS',
     'BAZEL_NODE_PATCH_REQUIRE',
     'BAZEL_NODE_RUNFILES_HELPER',
-    'BAZEL_PATCH_GUARDS',
-    'BAZEL_PATCH_ROOT',
+    'BAZEL_PATCH_ROOTS',
     'BAZEL_TARGET',
     'BAZEL_WORKSPACE',
     'BAZELISK_SKIP_WRAPPER',

commit 1d232d951fa51cf74b2460641d762f013c50a371

fix: update usages of `ExternalNpmPackageInfo.path` to be safe and default to empty string
`ExternalNpmPackageInfo` had a `path` attribute introduced and used in
2c2cc6eec42b0f4f805fb3063920107b78f821fd, but the provider was publicly exported beforehand. Even
though all internal usages were updated to include a path, this change was shipped in `3.2.0` and
introduced an unintentional breaking change for consumers. Most notably, `rules_postcss` manually
implements this provider and does not give a `path` failing in versions `3.2.0` and up.

https://github.com/bazelbuild/rules_postcss/blob/2bd16fda40cd4bf4fbf0b477b968366ec1602103/internal/plugin.bzl#L30-L41

Solution here is to safely retrieve the `path` attribute and default to empty string at all places
it is used. A test is also introduced to ensure that a `nodejs_binary()` can depend on a custom
`ExternalNpmPackageInfo` provider without a `path` attribute, which should hopefully prevent against
regressions.

internal/node/node.bzl
@@ -48,7 +48,7 @@ def _compute_node_modules_roots(ctx):
     # Add in roots from third-party deps
     for d in ctx.attr.data:
         if ExternalNpmPackageInfo in d:
-            path = d[ExternalNpmPackageInfo].path
+            path = getattr(d[ExternalNpmPackageInfo], "path", "")
             workspace = d[ExternalNpmPackageInfo].workspace
             if path in node_modules_roots:
                 other_workspace = node_modules_roots[path]

internal/providers/external_npm_package_info.bzl
@@ -45,7 +45,7 @@ def _node_modules_aspect_impl(target, ctx):
         for dep in ctx.rule.attr.deps:
             if ExternalNpmPackageInfo in dep:
                 has_directories = has_directories or dep[ExternalNpmPackageInfo].has_directories
-                path = dep[ExternalNpmPackageInfo].path
+                path = getattr(dep[ExternalNpmPackageInfo], "path", "")
                 workspace = dep[ExternalNpmPackageInfo].workspace
                 sources_depsets = []
                 if path in paths:

internal/providers/node_runtime_deps_info.bzl
@@ -50,7 +50,7 @@ def _compute_node_modules_roots(ctx):
         deps += ctx.attr.deps
     for d in deps:
         if ExternalNpmPackageInfo in d:
-            path = d[ExternalNpmPackageInfo].path
+            path = getattr(d[ExternalNpmPackageInfo], "path", "")
             workspace = d[ExternalNpmPackageInfo].workspace
             if path in node_modules_roots:
                 other_workspace = node_modules_roots[path]

internal/linker/test/default_path/BUILD.bazel
@@ -1,0 +1,30 @@
+load("//:index.bzl", "generated_file_test", "nodejs_binary", "npm_package_bin")
+load(
+    ":custom_external_npm_package_info.bzl",
+    "custom_external_npm_package_info",
+)
+
+package(default_testonly = True)
+
+# Custom target returning `ExternalNpmPackageInfo` without a `path` field.
+custom_external_npm_package_info(
+    name = "external_npm_package_info",
+)
+
+nodejs_binary(
+    name = "binary",
+    data = [":external_npm_package_info"],
+    entry_point = "default_path.js",
+)
+
+npm_package_bin(
+    name = "generated",
+    stdout = "generated.txt",
+    tool = ":binary",
+)
+
+generated_file_test(
+    name = "test",
+    src = "golden.txt",
+    generated = "generated.txt",
+)

internal/linker/test/default_path/custom_external_npm_package_info.bzl
@@ -1,0 +1,30 @@
+"""Custom `ExternalNpmPackageInfo` target without a `path` attribute."""
+
+load(
+    "//internal/providers:external_npm_package_info.bzl",
+    "ExternalNpmPackageInfo",
+)
+
+def _custom_external_npm_package_info_impl(ctx):
+    return ExternalNpmPackageInfo(
+        direct_sources = depset([], transitive = ctx.files.deps),
+        sources = depset(ctx.files.srcs, transitive = ctx.files.deps),
+        has_directories = False,
+        workspace = "npm",
+        # `path` is intentionally **not** provided.
+        # Historical note: `ExternalNpmPackageInfo` was publicly exported prior
+        # to the `path` attribute's introduction. This means that we cannot
+        # assume an `ExternalNpmPackageInfo` has a `path`, and must safely check
+        # it each time.
+    )
+
+custom_external_npm_package_info = rule(
+    implementation = _custom_external_npm_package_info_impl,
+    attrs = {
+        "srcs": attr.label_list(
+            allow_files = True,
+            default = [],
+        ),
+        "deps": attr.label_list(default = []),
+    },
+)

internal/linker/test/default_path/default_path.js
@@ -1,0 +1,1 @@
+console.log('ExternalNpmPackageInfo with no path works!');