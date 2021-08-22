010d12f5884e6da1b68450c6475929813825fbf4
Douglas Parker <dgp1130@users.noreply.github.com>
2021-08-21 20:04:43 -0700

    fix: update Skylib hash
    Skylib appears to be using the same hash as `rules_nodejs`, which definitely isn't right. Updated to use the hash referenced in [Skylib's 1.0.3 release notes](https://github.com/bazelbuild/bazel-skylib/releases/tag/1.0.3).

--- packages/create/index.js
+++ packages/create/index.js
@@ -168,7 +168,7 @@ def fetch_dependencies():
             "https://github.com/bazelbuild/bazel-skylib/releases/download/1.0.3/bazel-skylib-1.0.3.tar.gz",
             "https://mirror.bazel.build/github.com/bazelbuild/bazel-skylib/releases/download/1.0.3/bazel-skylib-1.0.3.tar.gz",
         ],
-        sha256 = "275744d287af4c3a78d7c9891f2d970b7bc7eca8cfc0e9a671fe6258d09ff217",
+        sha256 = "1c531376ac7e5a180e0237938a2536de0c54d93f5c278634818e0efc952dd56c",
     )`
   let workspaceContent = `# Bazel workspace created by @bazel/create 0.0.0-PLACEHOLDER
 
