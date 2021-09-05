018fe24e17dd9fff957f3e2fc7f2fd4e260e8baa
Sahin <thesayyn@gmail.com>
2021-09-05 19:36:01 +0200

    test: remove worker test and realy on e2e

--- packages/worker/test/work/worker.ts
+++
@@ -1,9 +0,0 @@
-
-import { runWorkerLoop } from "@bazel/worker";
-import { copyFileSync } from "fs";
-
-runWorkerLoop(async (args, inputs) => {
-    const [output, input] = args;
-    copyFileSync(input, output);
-    return true;
-});
