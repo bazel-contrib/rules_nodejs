09da6e387bb74cf28f09f2e20b3a375f18e3d969
Jason Bedard <jason+github@jbedard.ca>
2021-09-28 13:25:50 -0700

    test(rollup): add ts_library, ts_project, alias-mapping tests (#2722)

---
+++ packages/rollup/test/ts_library/rollup.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  onwarn: (warning) => {
+    // Always fail on warnings, assuming we don't know which are harmless.
+    // We can add exclusions here based on warning.code, if we discover some
+    // types of warning should always be ignored under bazel.
+    throw new Error(warning.message);
+  },
+};
---
+++ packages/rollup/test/ts_project/relative-module/lib.ts
@@ -0,0 +1,1 @@
+export const getId = () => 'relative-module';
