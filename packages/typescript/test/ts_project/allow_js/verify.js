bd8b7f362d21cf45947f202ffff08febe15aec09
Alex Eagle <eagle@post.harvard.edu>
2021-10-28 09:58:03 -0700

    fix(typescript): don't declare outputs that collide with inputs (#3046)

--- packages/typescript/test/ts_project/allow_js/verify.js
+++ packages/typescript/test/ts_project/allow_js/verify.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 
-const types_files = process.argv.slice(2, 4);
-const code_file = process.argv[4];
-assert.ok(types_files.some(f => f.endsWith('out/a.d.ts')), 'Missing a.d.ts');
-assert.ok(types_files.some(f => f.endsWith('out/a.d.ts.map')), 'Missing a.d.ts.map');
-assert.ok(code_file.endsWith('out/a.js'), 'Missing a.js');
+const output_files = process.argv.slice(2);
+for (const file of ['a', 'b']) {
+    assert.ok(output_files.some(f => f.endsWith(`out/${file}.d.ts`)), `Missing ${file}.d.ts`);
+    assert.ok(output_files.some(f => f.endsWith(`out/${file}.d.ts.map`)), `Missing ${file}.d.ts.map`);
+    assert.ok(output_files.some(f => f.endsWith(`out/${file}.js`)), `Missing ${file}.js`);
+}
