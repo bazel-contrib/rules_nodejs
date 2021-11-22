3c44f51005461633461941ec3c1ede9803c568ad
Greg Magolan <gmagolan@gmail.com>
2021-11-22 14:11:33 -0800

    fix: normalize module path passed to runfiles helper for robustness (#3094)

--- packages/runfiles/test/runfile_resolution.spec.js
+++ packages/runfiles/test/runfile_resolution.spec.js
@@ -1,4 +1,4 @@
-const {join} = require('path')
+const {join, dirname} = require('path')
 const {runfiles} = require('@bazel/runfiles');
 
 describe('runfile resolution', () => {
@@ -9,6 +9,38 @@ describe('runfile resolution', () => {
 
     expect(normalizePath(testFixturePath)).toEqual(normalizePath(expectedPath),
             'Expected the test fixture to be resolved next to the spec source file.');
+  });
+
+  it('should properly resolve with forward slashes', () => {
+    const testFixturePath = runfiles.resolve('build_bazel_rules_nodejs\\packages\\runfiles\\test\\test_fixture.md');
+    const expectedPath = join(__dirname, 'test_fixture.md');
+
+    expect(normalizePath(testFixturePath)).toEqual(normalizePath(expectedPath),
+            'Expected the test fixture to be resolved next to the spec source file.');
+  });
+
+  it('should properly resolve a subdirectory of a runfile', () => {
+    const packagePath = runfiles.resolve('build_bazel_rules_nodejs/packages');
+    // Alternate with trailing slash
+    const packagePath2 = runfiles.resolve('build_bazel_rules_nodejs/packages/');
+    const expectedPath = dirname(dirname(dirname(runfiles.resolve('build_bazel_rules_nodejs/packages/runfiles/test/test_fixture.md.generated_file_suffix'))));
+
+    expect(normalizePath(packagePath)).toEqual(normalizePath(expectedPath),
+      'Expected to resolve a subdirectory of a runfile.');
+    expect(normalizePath(packagePath2)).toEqual(normalizePath(expectedPath),
+      'Expected to resolve a subdirectory of a runfile.');
+  });
+
+  it('should properly resolve the workspace root of a runfile', () => {
+    const packagePath = runfiles.resolve('build_bazel_rules_nodejs');
+    // Alternate with trailing slash
+    const packagePath2 = runfiles.resolve('build_bazel_rules_nodejs/');
+    const expectedPath = dirname(dirname(dirname(dirname(runfiles.resolve('build_bazel_rules_nodejs/packages/runfiles/test/test_fixture.md.generated_file_suffix')))));
+
+    expect(normalizePath(packagePath)).toEqual(normalizePath(expectedPath),
+      'Expected to resolve the workspace root of a runfile.');
+    expect(normalizePath(packagePath2)).toEqual(normalizePath(expectedPath),
+      'Expected to resolve the workspace root of a runfile.');
   });
 });
 
