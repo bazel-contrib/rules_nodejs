f7f1e97be90c06d056f1389503cd49447c5c75eb
Greg Magolan <gmagolan@gmail.com>
2021-12-10 09:54:26 -0800

    chore: cleanup duplicate symbols in WORKSAPCE file

--- internal/node/test/toolchain_test_v16.js
+++ internal/node/test/toolchain_test_v16.js
@@ -1,4 +1,4 @@
-const version = 'v16.9.0';
+const version = 'v16.5.0';
 if (version !== process.version) {
   console.error(`there is a mismatch with the nodejs toolchain called and used for nodejs_binary: expected: ${version}, found: ${process.version}`);
   process.exitCode = 1;
