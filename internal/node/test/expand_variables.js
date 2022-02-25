46dde62c54cbe04687776dd3ab153a84bd69929f
Derek Cormier <derek@aspect.dev>
2022-02-25 11:01:11 -0800

    feat(builtin): perform make variable substitution in npm_package_bin env vars

--- internal/node/test/expand_variables.js
+++ internal/node/test/expand_variables.js
@@ -1,4 +1,16 @@
 const fs = require('fs');
 const args = process.argv.slice(2);
-const outfile = args.shift();
-fs.writeFileSync(outfile, JSON.stringify(args, null, 2), 'utf-8');
+const outfile = args[0];
+const dump = {
+    args,
+    env: {
+        OUTFILE: process.env.OUTFILE,
+        COMPLATION_MODE: process.env.COMPLATION_MODE,
+        TARGET_CPU: process.env.TARGET_CPU,
+        BINDIR: process.env.BINDIR,
+        SOME_TEST_ENV: process.env.SOME_TEST_ENV,
+        SOMEARG$$: process.env.SOMEARG$$,
+        SOME0ARG: process.env.SOME0ARG,
+    }
+}
+fs.writeFileSync(outfile, JSON.stringify(dump, null, 2), 'utf-8');
