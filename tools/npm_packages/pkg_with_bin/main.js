5bf3782a72a6a234b075be3f913a257c2cf24bb7
Sahin Yort <thesayyn@gmail.com>
2022-04-01 10:33:43 +0300

    fix: exports_directories_only causes node to resolve from runfiles/node_modules (#3380)

---
+++ tools/npm_packages/pkg_with_bin/main.js
@@ -0,0 +1,3 @@
+globalThis["node_resolve_main_resolved_path_by_pkg_with_bin"] = require.resolve("node_resolve_main");
+require("instanceof_test_execroot_lib");
+// See: internal/node/test/instanceof_test_execroot
