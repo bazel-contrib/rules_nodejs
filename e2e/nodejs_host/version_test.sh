b222c8d3326e48c5bc9a31fd6597aa0e12b65d03
matt <matthew.jh@outlook.com>
2022-04-27 23:11:26 +0100

    fix(builtin): add npx target that uses repo node

--- e2e/nodejs_host/version_test.sh
+++ e2e/nodejs_host/version_test.sh
@@ -11,10 +11,25 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
 # --- end runfiles.bash initialization v2 ---
 
+# `--verbose` output looks like below - idea is to search within it for node version and path
+#npm verb cli [
+#npm verb cli   '/usr/local/bin/node',
+#npm verb cli   '/private/var/tmp/_bazel_matt/bfec7e36d4722e7aa5af2167c4384e14/external/nodejs_darwin_arm64/bin/nodejs/lib/node_modules/npm/bin/npm-cli.js',
+#npm verb cli   'exec',
+#npm verb cli   '--loglevel',
+#npm verb cli   'verbose'
+#npm verb cli ]
+
+echo $(rlocation "$1/node")
+echo $(rlocation "$1/$2")
+
 readonly EXPECTED_NODE_VERSION=$($(rlocation "$1/node") --version 2>&1)
 readonly OUT=$($(rlocation "$1/$2") --verbose 2>&1)
 
 echo "-----"
+echo "expected node version from node --version..."
+echo $EXPECTED_NODE_VERSION
+echo "$1/$2 --verbose stdout..."
 echo $OUT
 echo "-----"
 
