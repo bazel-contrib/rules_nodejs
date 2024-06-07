fcb8efafe4cdad69d20181d58c8dd6b9ea9481f2
Greg Magolan <greg@aspect.dev>
2024-06-06 22:47:16 -0700

    chore: fixup release notes prep

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -8,8 +8,8 @@ TAG=${GITHUB_REF_NAME}
 # The prefix is chosen to match what GitHub generates for source archives
 PREFIX="rules_nodejs-${TAG:1}"
 ARCHIVE="rules_nodejs-$TAG.tar.gz"
-git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
-SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
+git archive --format=tar --prefix="${PREFIX}/" "${TAG}" | gzip > "$ARCHIVE"
+SHA=$(shasum -a 256 "$ARCHIVE" | awk '{print $1}')
 
 cat << EOF
 ## Using Bzlmod with Bazel 6
@@ -23,7 +23,7 @@ bazel_dep(name = "rules_nodejs", version = "${TAG:1}")
 
 ## Using WORKSPACE
 
-Paste this snippet into your `WORKSPACE.bazel` file:
+Paste this snippet into your \`WORKSPACE.bazel\` file:
 
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
@@ -37,4 +37,4 @@ EOF
 
 # TODO: fill in more snippet if/when we have an e2e that illustrates what is needed
 # awk 'f;/--SNIP--/{f=1}' e2e/smoke/WORKSPACE.bazel
-echo "\`\`\`" 
+echo "\`\`\`"
